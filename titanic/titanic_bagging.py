f7ede1d0032bfaf02d5ba25a724b818f3e5600d4
Stephen Kong <zollen.kong@gmail.com>
2020-08-16 10:08:42 -0400

    code improvement

--- titanic/titanic_bagging.py
+++ titanic/titanic_bagging.py
@@ -11,6 +11,7 @@ Created on Aug. 16, 2020
 
 import os
 from pathlib import Path
+import numpy as np
 import pandas as pd
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn import preprocessing
@@ -33,6 +34,7 @@ out of it.
 
 """
  
+np.random.seed(87)
 
 label_column = [ 'survived' ]
 numeric_columns = [ 'age', 'fare' ]
@@ -42,38 +44,40 @@ all_features_columns = numeric_columns + categorical_columns
 
 PROJECT_DIR=str(Path(__file__).parent.parent)  
 train_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/train.csv'))
+test_df = pd.read_csv(os.path.join(PROJECT_DIR , 'data/eval.csv'))
 
 for name in categorical_columns + label_column:
     encoder = preprocessing.LabelEncoder()
         
-    keys = train_df[name].unique()
+    keys = np.union1d(train_df[name].unique(), test_df[name].unique())
    
     if len(keys) == 2:
         encoder = preprocessing.LabelBinarizer()
         
     encoder.fit(keys)
     train_df[name] = encoder.transform(train_df[name].values)
+    test_df[name] = encoder.transform(test_df[name].values)
 
 
 model1 = QuadraticDiscriminantAnalysis()
 model1.fit(train_df[all_features_columns], train_df[label_column])
-preds = model1.predict(train_df[all_features_columns])
+preds = model1.predict(test_df[all_features_columns])
 print("============ LQA  =============")
-print("Accuracy: ", round(accuracy_score(train_df[label_column], preds), 2))
-print("Precision: ", round(precision_score(train_df[label_column], preds), 2))
-print("Recall: ", round(recall_score(train_df[label_column], preds), 2))
-print('AUC-ROC:', round(roc_auc_score(train_df[label_column], preds), 2))
-print("Log Loss: ", round(log_loss(train_df[label_column], preds), 2))
-print(confusion_matrix(train_df[label_column], preds))
+print("Accuracy: ", round(accuracy_score(test_df[label_column], preds), 2))
+print("Precision: ", round(precision_score(test_df[label_column], preds), 2))
+print("Recall: ", round(recall_score(test_df[label_column], preds), 2))
+print('AUC-ROC:', round(roc_auc_score(test_df[label_column], preds), 2))
+print("Log Loss: ", round(log_loss(test_df[label_column], preds), 2))
+print(confusion_matrix(test_df[label_column], preds))
 
 model2 = BaggingClassifier(base_estimator=QuadraticDiscriminantAnalysis(), 
-                        n_estimators=15)
+                        n_estimators=38)
 model2.fit(train_df[all_features_columns], train_df[label_column])
-preds = model2.predict(train_df[all_features_columns])
+preds = model2.predict(test_df[all_features_columns])
 print("============ Bagging with LQA  =============")
-print("Accuracy: ", round(accuracy_score(train_df[label_column], preds), 2))
-print("Precision: ", round(precision_score(train_df[label_column], preds), 2))
-print("Recall: ", round(recall_score(train_df[label_column], preds), 2))
-print('AUC-ROC:', round(roc_auc_score(train_df[label_column], preds), 2))
-print("Log Loss: ", round(log_loss(train_df[label_column], preds), 2))
-print(confusion_matrix(train_df[label_column], preds))
+print("Accuracy: ", round(accuracy_score(test_df[label_column], preds), 2))
+print("Precision: ", round(precision_score(test_df[label_column], preds), 2))
+print("Recall: ", round(recall_score(test_df[label_column], preds), 2))
+print('AUC-ROC:', round(roc_auc_score(test_df[label_column], preds), 2))
+print("Log Loss: ", round(log_loss(test_df[label_column], preds), 2))
+print(confusion_matrix(test_df[label_column], preds))
