e6a92a5307ffa4032325df813fb7455b61c4fe22
Stephen Kong <zollen.kong@gmail.com>
2020-08-23 18:16:33 -0400

    perf tuning

--- titanic/titanic_gradboost1.py
+++ titanic/titanic_gradboost1.py
@@ -69,9 +69,9 @@ model = DecisionTreeClassifier()
 model.fit(train_df[all_features_columns], labels)
 print(np.stack((all_features_columns, func(model.feature_importances_)), axis=1))
 
-#numeric_columns = [ 'fare' ]
-#categorical_columns = [ 'sex', 'class', 'deck', 'alone' ]
-#all_features_columns = numeric_columns + categorical_columns
+numeric_columns = [ 'fare' ]
+categorical_columns = [ 'sex', 'class', 'deck', 'alone' ]
+all_features_columns = numeric_columns + categorical_columns
 
 model = GradientBoostingClassifier(n_estimators=200, min_samples_split= 10, min_samples_leaf=2, max_depth=17)
 model.fit(train_df[all_features_columns], train_df[label_column])
