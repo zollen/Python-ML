949272fb5c0d46de9f5e702eef8225d900a17054
Stephen Kong <zollen.kong@gmail.com>
2020-08-23 13:40:23 -0400

    perf tuning with processed data

--- titanic/titanic_logistic2.py
+++ titanic/titanic_logistic2.py
@@ -17,6 +17,7 @@ from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
+from sklearn.metrics import log_loss
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score
@@ -52,8 +53,8 @@ all_features_columns = numeric_columns + categorical_columns
 
 
 PROJECT_DIR=str(Path(__file__).parent.parent)
-train_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/train.csv'))
-test_df = pd.read_csv(os.path.join(PROJECT_DIR , 'data/eval.csv'))
+train_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/train_processed.csv'))
+test_df = pd.read_csv(os.path.join(PROJECT_DIR , 'data/eval_processed.csv'))
 
 train_df['family'] = train_df['n_siblings_spouses'] + train_df['parch']
 test_df['family'] = test_df['n_siblings_spouses'] + test_df['parch']
@@ -143,18 +144,23 @@ model.fit(train_df[all_features_columns], labels)
 
 print("================= TRAINING DATA =====================")
 preds = model.predict(train_df[all_features_columns])
-print("Accuracy: ", round(accuracy_score(train_df[label_column], preds), 2))
-print("Precision: ", round(precision_score(train_df[label_column], preds), 2))
-print("Recall: ", round(recall_score(train_df[label_column], preds), 2))
+print("Accuracy: %0.2f" % accuracy_score(train_df[label_column], preds))
+print("Precision: %0.2f" % precision_score(train_df[label_column], preds))
+print("Recall: %0.2f" % recall_score(train_df[label_column], preds))
+print("AUC-ROC: %0.2f" % roc_auc_score(train_df[label_column], preds))
+print("Log Loss: %0.2f" % log_loss(train_df[label_column], preds))
 print(confusion_matrix(train_df[label_column], preds))
 
 print("================= TEST DATA =====================")
 preds = model.predict(test_df[all_features_columns])
-print("Accuracy: ", round(accuracy_score(test_df[label_column], preds), 2))
-print("Precision: ", round(precision_score(test_df[label_column], preds), 2))
-print("Recall: ", round(recall_score(test_df[label_column], preds), 2))
+print("Accuracy: %0.2f" % accuracy_score(test_df[label_column], preds))
+print("Precision: %0.2f" % precision_score(test_df[label_column], preds))
+print("Recall: %0.2f" % recall_score(test_df[label_column], preds))
+print("AUC-ROC: %0.2f" % roc_auc_score(test_df[label_column], preds))
+print("Log Loss: %0.2f" % log_loss(test_df[label_column], preds))
 print(confusion_matrix(test_df[label_column], preds))
 print(classification_report(test_df[label_column], preds))
+
 
 plot_roc(model, test_df[all_features_columns].values, test_df[label_column].values)
 #plt.show()
