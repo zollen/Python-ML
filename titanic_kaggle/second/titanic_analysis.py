9914d7e2014dcc263a36aafb7fa7d6d3f6bdfc7c
Stephen Kong <zollen.kong@gmail.com>
2020-08-31 08:30:00 -0400

    work in progress

--- titanic_kaggle/second/titanic_analysis.py
+++ titanic_kaggle/second/titanic_analysis.py
@@ -6,25 +6,13 @@ Created on Aug. 1, 2020
 
 import os
 from pathlib import Path
-import random
 import numpy as np
 import pandas as pd
 import re
 import seaborn as sb
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import RandomizedSearchCV
 from sklearn import preprocessing
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import GroupKFold, RepeatedStratifiedKFold
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import log_loss
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
+
 import warnings
 
 
@@ -63,68 +51,6 @@ title_category = {
         "Girl.": "Girl",
         "Nurse.": "Nurse"
     }
-
-def groups_generator(total, k, seed):
-    
-    groups = []
-    
-    done = False
-    while done == False:
-        for item in range(0, k):
-            groups.append(item)
-            if len(groups) == total:
-                done = True
-                break
-            
-    random.seed(seed)
-    random.shuffle(groups)
-    
-    return groups
-    
-def cross_validation(df, labels, k, repeats, seeds):
-    
-    tbl = {}
-    tbl['accuracy'], tbl['precision'], tbl['recall'], tbl['auc'],tbl['loss'] = [], [], [], [], []
-        
-    for i in range(0, repeats):
-        kfold = GroupKFold(n_splits = k)
-        groups = groups_generator(len(df), k, seeds[i])
-        for train_index, test_index in kfold.split(df, groups = groups):
-            x_train, y_train = df.iloc[train_index], labels.iloc[train_index]
-            x_test, y_test = df.iloc[test_index], labels.iloc[test_index]
-            classifier = QuadraticDiscriminantAnalysis(reg_param = 0.1, tol = 0.0003)
-            classifier.fit(x_train, y_train)
-            preds = classifier.predict(x_test)
-            a1, a2, a3, a4, a5 = score_model(y_test, preds)
-            tbl['accuracy'].append(a1)
-            tbl['precision'].append(a2)
-            tbl['recall'].append(a3)
-            tbl['auc'].append(a4)
-            tbl['loss'].append(a5)
-            
-    a1, a2, a3, a4, a5 = np.mean(tbl['accuracy']), np.mean(tbl['precision']), np.mean(tbl['recall']), np.mean(tbl['auc']), np.mean(tbl['loss'])
-    
-    display_score(a1, a2, a3, a4, a5)
-    
-    return a1, a2, a3, a4, a5
-            
-    
-def score_model(observed, predicted):
-    accuracy = accuracy_score(observed, predicted)
-    precision = precision_score(observed, predicted)
-    recall = recall_score(observed, predicted)
-    auc = roc_auc_score(observed, predicted)
-    loss = log_loss(observed, predicted)
-    
-    return accuracy, precision, recall, auc, loss
-
-def display_score(accur, prec, recal, auc, loss):
-    print("Accuracy: %0.2f    Precision: %0.2f    Recall: %0.2f    AUC: %0.2f    Loss: %0.2f" %
-          (accur, prec, recal, auc, loss))
-    
-def show_score (observed, predicted):
-    print("Accuracy: %0.2f    Precision: %0.2f    Recall: %0.2f    AUC: %0.2f    Loss: %0.2f" %
-           (score_model(observed, predicted)))
     
 def map_title(rec):
     title = title_category[rec['Title']]
@@ -174,6 +100,29 @@ def normalize(df, columns):
             
     return pdf
 
+def fill_by_regression(df_src, df_dest, name, columns):
+ 
+    input_columns = columns
+    predicted_columns = [ name ]
+
+    withVal = df_src[df_src[name].isna() == False]
+    withoutVal = df_src[df_src[name].isna() == True]
+    
+    cat_columns = set(input_columns).intersection(categorical_columns)
+
+    df1 = normalize(withVal, cat_columns)
+    df2 = normalize(withoutVal, cat_columns)
+    
+    model = DecisionTreeClassifier()
+    model.fit(df1[input_columns], withVal[predicted_columns])
+
+    preds = model.predict(df2[input_columns])
+    preds = [ round(i, 0) for i in preds ]
+    print("Predicted %s values: " % name)
+    print(np.stack((withoutVal['PassengerId'], preds), axis=1))
+
+    df_dest.loc[df_dest[name].isna() == True, name] = preds
+
 def fill_by_classification(df_src, df_dest, name, columns):
 
     input_columns = columns
@@ -192,7 +141,7 @@ def fill_by_classification(df_src, df_dest, name, columns):
     preds = model.predict(df2[input_columns])
     df_dest.loc[df_dest[name].isna() == True, name] = preds
     
-def process(df):
+def enginneering(df):
     
     df['Title'] = df['Name'].apply(lambda x : re.search('[a-zA-Z]+\\.', x).group(0))
     df['Title'] = df.apply(map_title, axis = 1)
@@ -205,84 +154,18 @@ def process(df):
     for title in set(title_category.values()):
         df.loc[((df['Age'].isna() == True) & (df['Title'] == title)), 'Age'] = df.loc[((df['Age'].isna() == False) & (df['Title'] == title)), 'Age'].median()
     df['Age'] = df['Age'].astype('int32')
-    
-    
-    for name in categorical_columns:
-        encoder = preprocessing.LabelEncoder()   
-        keys = df[name].unique()
-
-        if len(keys) == 2:
-            encoder = preprocessing.LabelBinarizer()
-        
-        encoder.fit(keys)
-        df[name] = encoder.transform(df[name].values)
-    
- 
-    labels = df[label_column]
-    
-    
-    ## Drop Age and Sex but Keep Title improve score
-    df.drop(columns=label_column, inplace=True)    
-    df.drop(columns=[ 'PassengerId', 'Age', 'Sex' ], inplace=True)
-
-    df = pd.get_dummies(df, columns=[ 'Title', 'Pclass', 'Embarked' ])
-
-
-    scaler = MinMaxScaler()   
-    cols = list(df.columns)
-    df[cols] = scaler.fit_transform(df[cols]) 
-    
-    return df, labels
+     
+    return df
 
 
 PROJECT_DIR=str(Path(__file__).parent.parent)  
 train_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/train.csv'))
 test_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/test.csv'))
 
-train_df, tlabels = process(train_df)
+train_df = enginneering(train_df)
+test_df = enginneering(test_df)
 
 print(train_df.head())
 
-
-if False:
-    param_grid = dict({ "reg_param": np.arange(0.0, 1.0, 0.1),
-                       "tol": [ 3.0e-4, 2.0e-4, 1.0e-4, 3.0e-3, 2.0e-3, 1.0e-3, 3.0e-2, 2.0e-2, 1.0e-2   ]
-                        })
-    model = RandomizedSearchCV(estimator = QuadraticDiscriminantAnalysis(), 
-                        param_distributions = param_grid, n_jobs=-1, n_iter=500)
-
-    model.fit(train_df, tlabels)
-
-    print("====================================================================================")
-    print("Best Score: ", model.best_score_)
-    print("Best reg_param: ", model.best_estimator_.reg_param)
-    print("Best tol: ", model.best_estimator_.tol)
-
-    exit()
-
-model = QuadraticDiscriminantAnalysis(reg_param = 0.1, tol = 0.0003)
-model.fit(train_df, tlabels)
-print("================= TRAINING DATA =====================")
-preds = model.predict(train_df)
-show_score(tlabels, preds)
-print(confusion_matrix(tlabels, preds))
-print(classification_report(tlabels, preds))
-
-print("================== CROSS VALIDATION ==================")
-kfold = RepeatedStratifiedKFold(n_splits = 9, n_repeats = 5, random_state = 87)
-results = cross_val_score(QuadraticDiscriminantAnalysis(reg_param = 0.1, tol = 0.0003), train_df, tlabels, cv = kfold)
-print("9-Folds Cross Validation Accuracy: %0.2f" % results.mean())
-
-tbl = {}
-tbl['accuracy'], tbl['precision'], tbl['recall'], tbl['auc'],tbl['loss'] = [], [], [], [], []
-        
-for k in range(4, 10):
-    a1, a2, a3, a4, a5 = cross_validation(train_df, tlabels, k, 3, [0, 23, 87])
-    tbl['accuracy'].append(a1)
-    tbl['precision'].append(a2)
-    tbl['recall'].append(a3)
-    tbl['auc'].append(a4)
-    tbl['loss'].append(a5)
-print("============================= AVERAGE ===========================")
-display_score(np.mean(tbl['accuracy']), np.mean(tbl['precision']), np.mean(tbl['recall']), 
-              np.mean(tbl['auc']), np.mean(tbl['loss']))
+train_df.to_csv(os.path.join(PROJECT_DIR, 'data/train_processed.csv'), index=False)
+test_df.to_csv(os.path.join(PROJECT_DIR, 'data/test_processed.csv'), index=False)
