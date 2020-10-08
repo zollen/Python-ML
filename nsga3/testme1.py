8dcee164203a7c169bd59309be5d77dd1f060af8
Stephen Kong <zollen.kong@gmail.com>
2020-10-08 16:35:14 -0400

    sample

---
+++ nsga3/testme1.py
@@ -0,0 +1,104 @@
+'''
+Created on Oct. 8, 2020
+
+@author: zollen
+'''
+
+import random
+from deap import base
+from deap import creator
+from deap import tools
+from deap import algorithms
+import numpy as np
+from pymop.factory import get_problem
+
+def factorial(val):
+    fact = 1
+    for i in range(1, val+1):
+        fact = fact * i
+        
+    return fact
+
+PROBLEM = "dtlz2"
+NOBJ = 3
+K = 10
+NDIM = NOBJ + K - 1
+P = 12
+H = factorial(NOBJ + P - 1) / (factorial(P) * factorial(NOBJ - 1))
+BOUND_LOW, BOUND_UP = 0.0, 1.0
+problem = get_problem(PROBLEM, n_var=NDIM, n_obj=NOBJ)
+
+MU = int(H + (4 - H % 4))
+NGEN = 400
+CXPB = 1.0
+MUTPB = 1.0
+
+ref_points = tools.uniform_reference_points(NOBJ, P)
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,) * NOBJ)
+creator.create("Individual", list, fitness=creator.FitnessMin)
+
+def uniform(low, up, size=None):
+    try:
+        return [random.uniform(a, b) for a, b in zip(low, up)]
+    except TypeError:
+        return [random.uniform(a, b) for a, b in zip([low] * size, [up] * size)]
+
+toolbox = base.Toolbox()
+toolbox.register("attr_float", uniform, BOUND_LOW, BOUND_UP, NDIM)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attr_float)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+toolbox.register("evaluate", problem.evaluate, return_values_of=["F"])
+toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=BOUND_LOW, up=BOUND_UP, eta=30.0)
+toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUND_LOW, up=BOUND_UP, eta=20.0, indpb=1.0/NDIM)
+toolbox.register("select", tools.selNSGA3, ref_points=ref_points)
+
+def main(seed=None):
+    random.seed(seed)
+
+    # Initialize statistics object
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", np.mean, axis=0)
+    stats.register("std", np.std, axis=0)
+    stats.register("min", np.min, axis=0)
+    stats.register("max", np.max, axis=0)
+
+    logbook = tools.Logbook()
+    logbook.header = "gen", "evals", "std", "min", "avg", "max"
+
+    pop = toolbox.population(n=MU)
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+
+    # Compile statistics about the population
+    record = stats.compile(pop)
+    logbook.record(gen=0, evals=len(invalid_ind), **record)
+    print(logbook.stream)
+
+    # Begin the generational process
+    for gen in range(1, NGEN):
+        offspring = algorithms.varAnd(pop, toolbox, CXPB, MUTPB)
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Select the next generation population from parents and offspring
+        pop = toolbox.select(pop + offspring, MU)
+
+        # Compile statistics about the new population
+        record = stats.compile(pop)
+        logbook.record(gen=gen, evals=len(invalid_ind), **record)
+        print(logbook.stream)
+
+    return pop, logbook
+
+if __name__ == "__main__":
+    main()
