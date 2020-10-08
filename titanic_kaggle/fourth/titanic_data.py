ad4237b2bfb47c784021073626a5c487976c4064
Stephen Kong <zollen.kong@gmail.com>
2020-10-08 15:09:38 -0400

    use the best code

--- titanic_kaggle/fourth/titanic_data.py
+++ titanic_kaggle/fourth/titanic_data.py
@@ -9,15 +9,56 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import pprint
-import titanic_kaggle.lib.titanic_lib as tb
+import seaborn as sb
 import warnings
+import titanic_kaggle.lib.titanic_lib as tb
 
 warnings.filterwarnings('ignore')
+
+    
+    
+
 pd.set_option('max_columns', None)
 pd.set_option('max_rows', None)
+pd.set_option('display.width', 1000)
 np.random.seed(0)
+sb.set_style('whitegrid')
 pp = pprint.PrettyPrinter(indent=3) 
 
+label_column = [ 'Survived']
+identity_columns = [ 'PassengerId', 'Ticket' ]
+numeric_columns = [ 'Age', 'Fare' ]
+categorical_columns = [ 'Sex', 'Title', 'Pclass', 'Embarked', 'Cabin' ]
+all_features_columns = numeric_columns + categorical_columns 
+
+
+def fillAge(src_df, dest_df):
+    
+    ages = src_df.groupby(['Title', 'Sex', 'SibSp', 'Parch'])['Age'].median()
+
+    for index, value in ages.items():
+            dest_df.loc[(dest_df['Age'].isna() == True) &
+                 (dest_df['Title'] == index[0]) & 
+                 (dest_df['Sex'] == index[1]) &
+                 (dest_df['SibSp'] == index[2]) &
+                 (dest_df['Parch'] == index[3]), 'Age'] = value
+                 
+    ages = src_df.groupby(['Title', 'Sex', 'SibSp'])['Age'].median()
+
+    for index, value in ages.items():
+        dest_df.loc[(dest_df['Age'].isna() == True) &
+                 (dest_df['Title'] == index[0]) & 
+                 (dest_df['Sex'] == index[1]) &
+                 (dest_df['SibSp'] == index[2]), 'Age'] = value
+               
+    ages = src_df.groupby(['Title', 'Sex'])['Age'].median()
+
+    for index, value in ages.items():
+        dest_df.loc[(dest_df['Age'].isna() == True) &
+                 (dest_df['Title'] == index[0]) & 
+                 (dest_df['Sex'] == index[1]), 'Age'] = value
+
+## pd.qcut() based boundaries yields better result
 def binAge(df):
     df.loc[df['Age'] < 14, 'Age'] = 7
     df.loc[(df['Age'] < 19) & (df['Age'] >= 14), 'Age'] = 17
@@ -30,7 +71,8 @@ def binAge(df):
     df.loc[(df['Age'] < 50) & (df['Age'] >= 41), 'Age'] = 46
     df.loc[(df['Age'] < 80) & (df['Age'] >= 50), 'Age'] = 70
     df.loc[df['Age'] >= 80, 'Age'] = 80
-    
+
+## pd.qcut() based boundaries yields better result    
 def binFare(df):
     df.loc[df['Fare'] < 7.229, 'Fare'] = 4
     df.loc[(df['Fare'] < 7.75) & (df['Fare'] >= 7.229), 'Fare'] = 6
@@ -44,29 +86,72 @@ def binFare(df):
     df.loc[(df['Fare'] < 33.308) & (df['Fare'] >= 26.55), 'Fare'] = 30
     df.loc[(df['Fare'] < 55.9) & (df['Fare'] >= 33.308), 'Fare'] = 45
     df.loc[(df['Fare'] < 83.158) & (df['Fare'] >= 55.9), 'Fare'] = 75
-    df.loc[(df['Fare'] < 512.329) & (df['Fare'] >= 83.158), 'Fare'] = 100
-    df.loc[df['Fare'] >= 300, 'Fare'] = 150
+    df.loc[(df['Fare'] < 1000) & (df['Fare'] >= 83.158), 'Fare'] = 100
+    
+def fillCabin(src_df, dest_df):
+    
+    df = dest_df.copy()
+    
+    binFare(df)
+      
+    cabins = src_df.groupby(['Title', 'Fare', 'Pclass', 'SibSp', 'Parch', 'Embarked', 'Cabin'])['Cabin'].count()
+    
+    for index, _ in cabins.items():
+        df.loc[(df['Cabin'].isna() == True) &
+                 (df['Title'] == index[0]) & 
+                 (df['Fare'] == index[1]) & 
+                 (df['Pclass'] == index[2]) &
+                 (df['SibSp'] == index[3]) &
+                 (df['Parch'] == index[4]) & 
+                 (df['Embarked'] == index[5]), 'Cabin'] = cabins[index[0], index[1], index[2], index[3], index[4], index[5]].idxmax()
+           
+    cabins = src_df.groupby(['Title', 'Fare', 'Pclass', 'SibSp', 'Parch', 'Cabin'])['Cabin'].count()
 
+    for index, _ in cabins.items():
+        df.loc[(df['Cabin'].isna() == True) &
+                 (df['Title'] == index[0]) & 
+                 (df['Fare'] == index[1]) & 
+                 (df['Pclass'] == index[2]) &
+                 (df['SibSp'] == index[3]) &
+                 (df['Parch'] == index[4]), 'Cabin'] = cabins[index[0], index[1], index[2], index[3], index[4]].idxmax()
+                   
+    cabins = src_df.groupby(['Title', 'Fare', 'Pclass', 'SibSp', 'Cabin'])['Cabin'].count()
 
-label_column = [ 'Survived']
-identity_columns = [ 'PassengerId', 'Ticket', 'Room' ]
-numeric_columns = [ 'Age', 'SibSp', 'Parch', 'Fare' ]
-categorical_columns = [ 'Title', 'Sex', 'Embarked', 'Pclass'  ]
-all_features_columns = numeric_columns + categorical_columns 
+    for index, _ in cabins.items():
+        df.loc[(df['Cabin'].isna() == True) &
+                 (df['Title'] == index[0]) & 
+                 (df['Fare'] == index[1]) & 
+                 (df['Pclass'] == index[2]) &
+                 (df['SibSp'] == index[3]), 'Cabin'] = cabins[index[0], index[1], index[2], index[3]].idxmax()
+                  
+    cabins = src_df.groupby(['Title', 'Fare', 'Pclass', 'Cabin'])['Cabin'].count()
 
+    for index, _ in cabins.items():
+        df.loc[(df['Cabin'].isna() == True) &
+                 (df['Title'] == index[0]) & 
+                 (df['Fare'] == index[1]) & 
+                 (df['Pclass'] == index[2]), 'Cabin'] = cabins[index[0], index[1], index[2]].idxmax()
+                   
+    cabins = src_df.groupby(['Title', 'Fare', 'Cabin'])['Cabin'].count()
 
+    for index, _ in cabins.items():
+        df.loc[(df['Cabin'].isna() == True) &
+                 (df['Title'] == index[0]) & 
+                 (df['Fare'] == index[1]), 'Cabin'] = cabins[index[0], index[1]].idxmax()
+                   
+    cabins = src_df.groupby(['Title', 'Cabin'])['Cabin'].count()
+
+    for index, _ in cabins.items():
+        df.loc[(df['Cabin'].isna() == True) &
+                 (df['Title'] == index[0]), 'Cabin'] = cabins[index[0]].idxmax()
+                 
+    df.loc[df['Cabin'].isna() == True, 'Cabin' ] = 'X'
+             
+    dest_df['Cabin'] = df['Cabin']
+    
 PROJECT_DIR=str(Path(__file__).parent.parent)  
 train_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/train.csv'))
 test_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/test.csv'))
-
-
-print(train_df.info())
-print("=============== STATS ===================")
-print(train_df.describe())
-print("============== Training Total NULL ==============")
-print(train_df.isnull().sum())
-print("============== SKEW =====================")
-print(train_df.skew())
 
 train_df.loc[train_df['Cabin'] == 'T', 'Cabin'] = 'A'
 train_df.loc[train_df['Embarked'].isna() == True, 'Embarked'] = 'S'
@@ -74,23 +159,57 @@ train_df.loc[train_df['Fare'].isna() == True, 'Fare'] = 7.25
 test_df.loc[test_df['Embarked'].isna() == True, 'Embarked'] = 'S' 
 test_df.loc[test_df['Fare'].isna() == True, 'Fare'] = 7.25
 
+
 tb.reeigneeringTitle(train_df)
 tb.reeigneeringTitle(test_df)
 
-tb.captures(train_df)
-tb.captures(test_df)
+train_df['Room']  = train_df['Cabin'].apply(tb.captureRoom)
+test_df['Room']  = test_df['Cabin'].apply(tb.captureRoom)
 
-tb.fill_by_regression(train_df, train_df, 'Age', [ 'Title', 'Survived', 'SibSp', 'Parch', 'Fare', 'Sex', 'Pclass', 'Embarked' ])
-tb.fill_by_classification(train_df, train_df, 'Cabin', [ 'Title', 'Survived', 'SibSp', 'Parch', 'Fare', 'Sex', 'Age', 'Pclass', 'Embarked' ])
+train_df['Cabin'] = train_df['Cabin'].apply(tb.captureCabin) 
+test_df['Cabin'] = test_df['Cabin'].apply(tb.captureCabin) 
 
-tb.fill_by_regression(tb.combine(train_df, test_df), test_df, 'Age', [ 'Title', 'SibSp', 'Parch', 'Fare', 'Sex', 'Pclass', 'Embarked' ])
-tb.fill_by_classification(tb.combine( train_df, test_df ), test_df, 'Cabin', [ 'Title', 'Age', 'SibSp', 'Parch', 'Embarked', 'Sex', 'Fare', 'Pclass' ])
+train_df['Title'] = train_df['Title'].map(tb.titles)
+test_df['Title'] = test_df['Title'].map(tb.titles)
+
+train_df['Sex'] = train_df['Sex'].map(tb.sexes)
+test_df['Sex'] = test_df['Sex'].map(tb.sexes)
+
+train_df['Embarked'] = train_df['Embarked'].map(tb.embarkeds)
+test_df['Embarked'] = test_df['Embarked'].map(tb.embarkeds)
+
+train_df['Ticket'] = train_df['Ticket'].apply(tb.captureTicketId)
+test_df['Ticket'] = test_df['Ticket'].apply(tb.captureTicketId)
+
+train_df['Ticket'] = np.log(train_df['Ticket'])
+test_df['Ticket'] = np.log(test_df['Ticket'])
+
+all_df = pd.concat( [ train_df, test_df ], ignore_index = True )
+
+fillAge(all_df, train_df)
+fillAge(all_df, test_df)
+fillAge(all_df, all_df)
+
+train_df['Age'] = train_df['Age'].astype('int32')
+test_df['Age'] = test_df['Age'].astype('int32')
+
+binFare(all_df)
+
+fillCabin(all_df, train_df)
+fillCabin(all_df, test_df)
+fillCabin(all_df, all_df)
+
+train_df['Cabin'] = train_df['Cabin'].map(tb.cabins)
+test_df['Cabin'] = test_df['Cabin'].map(tb.cabins)
+all_df['Cabin'] = all_df['Cabin'].map(tb.cabins)
 
 tb.reeigneeringFamilySize(train_df)
 tb.reeigneeringFamilySize(test_df)
+tb.reeigneeringFamilySize(all_df)
 
 tb.typecast(train_df)
 tb.typecast(test_df)
+
 
 ttrain_df = train_df.copy()
 ttest_df = test_df.copy()
@@ -113,6 +232,7 @@ tbl = {
 
 pp.pprint(tbl)
 
+
 tb.navieBayes(ttrain_df, tbl)
 columns = [ 'Title', 'Age', 'Sex', 'Pclass', 'Cabin', 'Size', 'Fare', 'Embarked' ]
 coeffs = { "Title": 1.7516, "Age": 0.6860, "Sex": 0.3505, "Pclass": 0.1558, 
@@ -123,16 +243,37 @@ tb.reeigneeringSurvProb(ttest_df, coeffs, columns )
 train_df['Chance'] = ttrain_df['Chance']
 test_df['Chance'] = ttest_df['Chance']
 
-train_df['Cabin'] = train_df['Cabin'] * 1000 + train_df['Room']
-test_df['Cabin'] = test_df['Cabin'] * 1000 + test_df['Room']
+
+"""
+1. Ticket, search for identical ticketId (1. ticket number alone, 2. ticket number and ticket prefix pro
+2. try SVM column with C = 150-200
+3. implement features importants plots 
+
+features_imp = pd.DataFrame()
+features_imp['feature'] = features_names
+features_imp['importance'] = model.feature_importances_
+features_imp.sort_values(by=['importance'], ascending=True, inplace=True)
+features_imp.set_index('feature', inplace=True)
+features_imp.plot(kind='barh', figsize=(20, 20))
 
 
-train_df.drop(columns = ['Name', 'Ticket', 'Title', 'Size', 'Room'], inplace = True)
-test_df.drop(columns = ['Name', 'Ticket', 'Title', 'Size', 'Room'], inplace = True)
+4. Build your own LogistcRegression with genetic optimizatio
 
-outputs = ['PassengerId', 'Survived', 'Age', 'SibSp', 'Parch', 'Fare', 'Sex', 'Embarked',  'Pclass', 'Cabin', 'Chance' ]
-train_df[outputs].to_csv(os.path.join(PROJECT_DIR, 'data/train_processed.csv'), index=False)
-outputs = ['PassengerId', 'Age', 'SibSp', 'Parch', 'Fare', 'Sex', 'Embarked',  'Pclass', 'Cabin', 'Chance' ]
-test_df[outputs].to_csv(os.path.join(PROJECT_DIR, 'data/test_processed.csv'), index=False)
+"""
+
+
+
+
+
+
+
+
+
+train_df.drop(columns = ['Name', 'Cabin', 'Title', 'Size', 'Room'], inplace = True)
+test_df.drop(columns = ['Name', 'Cabin', 'Title', 'Size', 'Room'], inplace = True)
+
+
+train_df.to_csv(os.path.join(PROJECT_DIR, 'data/train_processed.csv'), index=False)
+test_df.to_csv(os.path.join(PROJECT_DIR, 'data/test_processed.csv'), index=False)
 
 print("Done")
