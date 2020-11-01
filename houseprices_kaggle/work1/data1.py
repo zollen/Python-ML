80540f2e4a79aecb6aab9b08150c7f38f555b925
Stephen Kong <zollen.kong@gmail.com>
2020-10-31 20:54:42 -0400

    new competition

---
+++ houseprices_kaggle/work1/data1.py
@@ -0,0 +1,21 @@
+'''
+Created on Oct. 31, 2020
+
+@author: zollen
+'''
+
+import os
+from pathlib import Path
+import pandas as pd
+import warnings
+
+warnings.filterwarnings('ignore')
+pd.set_option('max_columns', None)
+pd.set_option('max_rows', None)
+pd.set_option('display.width', 1000)
+
+PROJECT_DIR=str(Path(__file__).parent.parent)  
+train_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/train.csv'))
+test_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/test.csv'))
+
+print(train_df.head())
