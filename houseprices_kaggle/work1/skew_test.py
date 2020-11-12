9f3575468a41a1dfb68a2509dae6b9e1b35f19fb
Stephen Kong <zollen.kong@gmail.com>
2020-11-12 13:35:11 -0500

    update skew and log

--- houseprices_kaggle/work1/skew_test.py
+++ houseprices_kaggle/work1/skew_test.py
@@ -19,23 +19,38 @@ sb.set_style("whitegrid")
 
 PROJECT_DIR=str(Path(__file__).parent.parent)  
 train_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/train.csv'))
+test_df = pd.read_csv(os.path.join(PROJECT_DIR, 'data/test.csv'))
 
+all_df = pd.concat([train_df, test_df])
 
-train_df['LogSalePrice'] = train_df['SalePrice'].apply(lambda x : np.log1p(x))
-train_df['BoxSalePrice'] = boxcox1p(train_df['SalePrice'], boxcox_normmax(train_df['SalePrice']))
-print("Skew('SalePrice'): ", skew(train_df['SalePrice'].values))
-print("Skew(Log1p('SalePrice')): ", skew(train_df['LogSalePrice'].values))
-print("Skew(BoxCox('SalePrice')): ", skew(train_df['BoxSalePrice'].values))
+print("===================== SKEW AND LOG TEST ======================")
+train_df['LogMiscVal'] = train_df['MiscVal'].apply(lambda x : np.log1p(x))
+train_df['BoxMiscVal'] = boxcox1p(train_df['MiscVal'], boxcox_normmax(train_df['MiscVal'] + 1))
+print("Skew(train_df['MiscVal']):                                   %0.4f" % skew(train_df['MiscVal'].values))
+print("Skew(Log1p(train_df'MiscVal'])):                             %0.4f" % skew(train_df['LogMiscVal'].values))
+print("Skew(BoxCox(train_df['MiscVal'], boxcox_normmax(train_df))): %0.4f" % skew(train_df['BoxMiscVal'].values))
+print()
+
+all_df['LogMiscVal'] = all_df['MiscVal'].apply(lambda x : np.log1p(x))
+all_df['BoxMiscVal'] = boxcox1p(all_df['MiscVal'], boxcox_normmax(all_df['MiscVal'] + 1))
+print("Skew(all_df['MiscVal']):                                     %0.4f" % skew(all_df['MiscVal'].values))
+print("Skew(Log1p(all_df['MiscVal'])):                              %0.4f" % skew(all_df['LogMiscVal'].values))
+print("Skew(BoxCox(all_df['MiscVal'])):                             %0.4f" % skew(all_df['BoxMiscVal'].values))
+
+print()
+train_df['AllMiscVal'] = boxcox1p(train_df['MiscVal'], boxcox_normmax(all_df['MiscVal'] + 1))
+print("Skew(BoxCox(train_df['MiscVal'], boxcox_normmax(all_df))):   %0.4f" % skew(train_df['AllMiscVal'].values))
+
 
 fig, (a1, a2, a3) = plt.subplots(1, 3)
 
 fig.set_size_inches(12 , 4)
 
-a1.set_title("SalePrice")
-sb.distplot(train_df['SalePrice'], fit = norm, ax = a1)
-a2.set_title("Log1p(SalePrice)")
-sb.distplot(train_df['LogSalePrice'], fit = norm, ax = a2)
-a3.set_title("BoxCox(SalePrice)")
-sb.distplot(train_df['BoxSalePrice'], fit = norm, ax = a3)
+a1.set_title("MiscVal")
+sb.distplot(train_df['MiscVal'], fit = norm, ax = a1)
+a2.set_title("Log1p(MiscVal)")
+sb.distplot(train_df['LogMiscVal'], fit = norm, ax = a2)
+a3.set_title("BoxCox(MiscVal)")
+sb.distplot(train_df['BoxMiscVal'], fit = norm, ax = a3)
 
 plt.show()
