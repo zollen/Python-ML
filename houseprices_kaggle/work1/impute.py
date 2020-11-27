928d71b5053da58a92b8a396314d96a2488fe17f
Stephen Kong <zollen.kong@gmail.com>
2020-11-26 21:38:39 -0500

    impute improvement

--- houseprices_kaggle/work1/impute.py
+++ houseprices_kaggle/work1/impute.py
@@ -10,10 +10,9 @@ import pandas as pd
 import pprint
 import warnings
 from sklearn.preprocessing import RobustScaler, LabelEncoder
-from sklearn.impute import KNNImputer
 from missingpy import MissForest
 from lightgbm import LGBMClassifier, LGBMRegressor
-from catboost import CatBoostClassifier, CatBoostRegressor
+
 
 
 
@@ -53,81 +52,9 @@ numeric_columns.remove('SalePrice')
 
 
 
-kk = []
-def fillValue(name, fid):
-    
-    global all_df
-    
-    num_columns = numeric_columns.copy()
-    cat_columns = categorical_columns.copy()
-            
-    if name in num_columns:
-        num_columns.remove(name)
-        
-    if name in cat_columns:
-        cat_columns.remove(name)
-    
-    all_ddf = all_df.copy()
-    
-    for colnam in cat_columns:
-        keys = all_df[colnam].unique()
-        
-        if np.nan in keys:
-            keys.remove(np.nan)
-            
-        vals = [ i  for i in range(0, len(keys))]
-        labs = dict(zip(keys, vals))
-        all_ddf[colnam] = all_ddf[colnam].map(labs)
-        
-    all_columns = num_columns + cat_columns
-            
-    '''
-    https://pypi.org/project/missingpy/
-    '''
-    imputer = MissForest()
-    all_ddf[all_columns] = imputer.fit_transform(all_ddf[all_columns])
-    all_ddf[all_columns] = all_ddf[all_columns].round(0).astype('int64')
-    
-
-    all_ddf = pd.get_dummies(all_ddf, columns = cat_columns) 
-    single_df = all_ddf[all_ddf['Id'] == fid]  
-    all_ddf = all_ddf[all_ddf[name].isna() == False]
-    
-
-    cat_columns = list(set(all_ddf.columns).symmetric_difference(num_columns + ['Id', 'SalePrice', name]))
-    cat_columns.sort(reverse = True)
-    all_columns = num_columns + cat_columns
-    
-    if all_df[name].dtypes == 'object':
-        keys = all_ddf[name].unique().tolist()
-
-        if np.nan in keys:
-            keys.remove(np.nan)
-        
-        vals = [ i  for i in range(0, len(keys))]
-        labs = dict(zip(keys, vals))
-        rlabs = dict(zip(vals, keys))
-        all_ddf[name] = all_ddf[name].map(labs)
-        model = CatBoostClassifier(random_state = 0, verbose = False)
-    else:
-        model = CatBoostRegressor(random_state = 0, verbose = False)
-        
-
-    model.fit(all_ddf[all_columns], all_ddf[name])
-    prediction = model.predict(single_df[all_columns])
-    
-    if all_df[name].dtypes == 'object':  
-        prediction = prediction[:, 0]
-        print("%4d[%12s] ===> %s" % (fid, name, rlabs[prediction[0]]))
-        all_df.loc[all_df['Id'] == fid, name] = rlabs[prediction[0]]
-        kk.append(rlabs[prediction[0]])
-    else:
-        all_df.loc[all_df['Id'] == fid, name] = prediction[0]
-        print("%4d[%12s] ===> %d" % (fid, name, prediction[0]))
-        kk.append(np.round(prediction[0], 0))
 
         
-def fillValue2(df, name):
+def fillValue(df, name):
     work_df = df.copy()
     cat_columns = categorical_columns.copy()
     num_columns = numeric_columns.copy()
@@ -151,7 +78,10 @@ def fillValue2(df, name):
         work_df.loc[work_df[col].isna() == False, col] = scaler.fit_transform(work_df.loc[work_df[col].isna() == False, col].values.reshape(-1, 1))
     
     
-    imputer = KNNImputer()
+    '''
+    https://pypi.org/project/missingpy/
+    '''
+    imputer = MissForest()
     work_df[columns] = imputer.fit_transform(work_df[columns])
 
     learn_df = work_df[work_df[name].isna() == False]
@@ -173,15 +103,30 @@ def fillValue2(df, name):
     
 
 
+kk  = all_df.isnull().sum()
+names = []
+sums = []
+for name, val in kk.items():
+    names.append(name)
+    sums.append(val)
 
-fillValue('GarageArea', 2577)
+damn_df = pd.DataFrame()
+damn_df['Name'] = names
+damn_df['Nulls'] = sums
+damn_df = damn_df[damn_df['Nulls'] > 0]
+damn_df.sort_values('Nulls', ascending = True, inplace = True)
 
-fillValue('Electrical', 1380)
+labels = damn_df['Name'].tolist()
+
+
+# features sorted by the least number of missing values to the largest number of missing values    
+for name in labels:
+    fillValue(all_df, name)
     
-fillValue('GarageFinish', 2127)
-fillValue('GarageYrBlt', 2127)
-fillValue('GarageFinish', 2577)
-fillValue('GarageYrBlt', 2577)
+print(all_df.isnull().sum())
+
+all_df.to_csv(os.path.join(PROJECT_DIR, 'data/all.csv'), index = False)
+
 
     
 '''
