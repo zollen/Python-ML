68a4bb241253f4e227ed84a93c348e7652a27d34
Stephen Kong <zollen.kong@gmail.com>
2020-12-16 11:54:22 -0500

    battle!!!

--- rps_kaggle/battle.py
+++ rps_kaggle/battle.py
@@ -15,6 +15,9 @@ player1 = rps.GMarkov(3, 6)
 player2 = rps.NOrderMarkov(3, 6)
 
 results = []
+win1 = 0
+win2 = 0
+
 for _ in range(0, 1000):
 
     move1 = player1.predict()
@@ -27,9 +30,9 @@ for _ in range(0, 1000):
     
     res = move1 - move2
     
-    if res == 0:
-        winner = "TIE"
-    elif res ==  1 or res == -1:
+    winner = "TIE"
+    
+    if res ==  1 or res == -1:
         if res > 0:
             winner = player1
         else:
@@ -39,9 +42,20 @@ for _ in range(0, 1000):
             winner = player2
         else:
             winner = player1
+            
+    if winner == player1:
+        win1 = win1 + 1
+    elif winner == player2:
+        win2 = win2 + 1
+
     
-    msg = "{:>8} | {:<8}  =>   {:^20}"
+    msg = "{:>8} | {:<8} => {:^20}"
     print(msg.format(SIGNS[move1], SIGNS[move2], winner.__str__()))
     results.append(msg.format(SIGNS[move1], SIGNS[move2], winner.__str__()))
 
-
+if win1 == win2:
+    print("BOTH TIE!!")
+elif win1 > win2:
+    print(player1.__str__() + " WON!!")
+else:
+    print(player2.__str__() + " WON!!")
