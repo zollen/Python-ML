f35ac1705da4546b3354dc3b8e3199bca8409020
Stephen Kong <zollen.kong@gmail.com>
2020-12-22 09:53:21 -0500

    code fix

--- rps_kaggle/lib/rps_lib.py
+++ rps_kaggle/lib/rps_lib.py
@@ -212,13 +212,16 @@ class Classifier(BaseAgent):
 class MClassifier(Classifier):
     
     def __init__(self, classifier, states = 3, window = 3, delay_process = 5, counter = None):
-        super().__init(classifier, states, window, delay_process, counter)
+        super().__init__(classifier, states, window, delay_process, counter)
         
     def buildtrain(self):
-        pass
+        self.data[self.row] = self.mines[self.row:self.row+self.window].tolist() + self.opponent[self.row:self.row+self.window].tolist()
+        self.results = np.append(self.results, self.opponent[-1])    
+        self.row = self.row + 1
     
     def buildtest(self):
-        pass
+        return np.array(self.mines[-self.window:].tolist() + self.opponent[-self.window:].tolist()).reshape(1, -1)
+  
     
         
         
