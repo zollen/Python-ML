66d2d11ef4e556a9e9d753f22a7191e3431aabbf
Stephen Kong <zollen.kong@gmail.com>
2020-12-25 15:19:39 -0500

    using beta distribution

--- rps_kaggle/battle.py
+++ rps_kaggle/battle.py
@@ -5,6 +5,7 @@ Created on Dec. 16, 2020
 '''
 
 import numpy as np
+import time
 from xgboost import XGBClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -13,7 +14,8 @@ from sklearn.svm import SVC
 import rps_kaggle.lib.rps_lib as rps
 import warnings
 from rps_kaggle.lib.rps_lib import StandardCounterMover
-from sklearn.linear_model.tests.test_passive_aggressive import MyPassiveAggressive
+
+
 
 
 
@@ -21,159 +23,101 @@ warnings.filterwarnings('ignore')
 
 SIGNS = ['ROCK', 'PAPER', 'SCISSORS']
 
+forest = rps.OClassifier(RandomForestClassifier(random_state = 23, n_estimators = 10), window = 10)
+#ada.counter = StandardCounterMover(ada)
+xgb = rps.OClassifier(XGBClassifier(random_state = 23, n_estimators = 10, eval_metric = 'logloss'), window = 10)
+#xgb.counter = StandardCounterMover(xgb)
 
-class Balancer:
+agents = {
+    rps.Randomer(): [1, 1],
+    rps.MirrorOpponentDecider(ahead = 0): [1, 1],
+    rps.MirrorOpponentDecider(ahead = 1): [1, 1],
+    rps.MirrorOpponentDecider(ahead = 2): [1, 1],
+    rps.MirrorSelfDecider(ahead = 0): [1, 1],
+    rps.MirrorSelfDecider(ahead = 1): [1, 1],
+    rps.MirrorSelfDecider(ahead = 2): [1, 1],
+    forest: [1, 1],
+    xgb: [1, 1]
+    }
+
+class Agency:
     
-    def __init__(self, agents, duration):
+    def __init__(self, agents):
         self.agents = agents
-        self.duration = duration
-        self.interval = 0
-        self.target = None
-        
-    def __str__(self):
-        return self.target.__class__.__name__
-        
-    def add(self, token): 
-        for agent in agents:
-            agent.add(token)
-    
-    def submit(self, token):
-        for agent in agents:
-            agent.submit(token)             
-        return token
-    
-    def reset(self):        
-        for agent in agents:
-            agent.reset()           
-        self.target = None
-        self.interval = 0
-        
-    def decide(self):
-        if self.interval <= 0:
-            self.interval = self.duration
-            
-            targets = self.agents.copy()
-            if self.target != None:
-                targets.remove(self.target)
-                
-            self.target = self.agents[np.random.randint(0, len(targets))]
-            
-        self.interval = self.interval - 1
-        
-        return self.submit(self.target.decide())
-
-
-        
-class Distractor:   
-    
-    def __init__(self, primary, best, distractors, delta = 5):
-        self.primary = primary
-        self.best = best
-        self.distractors = distractors
-        self.delta = delta
         self.mines = []
         self.opponent = []
-        self.totalMatches = 0
-        self.totalWin = 0
-        self.totalLoss = 0
         self.executor = None
-        
+    
     def __str__(self):
-        return self.executor.__str__()
-        
-    def add(self, token): 
-        self.distractors.add(token)
-        self.primary.add(token)
-        self.best.add(token)
-        
-        self.opponent.append(token)
+        return "Agency(" + self.executor.__str__() + ")"
+    
+    def add(self, token):
+        for agent, _ in self.agents.items():
+            agent.add(token)
             
     def submit(self, token):
-        if self.executor != self.distractors:
-            self.distractors.submit(token)
-        if self.executor != self.primary:
-            self.primary.submit(token)
-        if self.executor != self.best:
-            self.best.submit(token)
-            
-        self.mines.append(token)
+        for agent, _ in self.agents.items():
+            if self.executor != agent:
+                agent.submit(token)
             
         return token
     
-    def reset(self):
-        self.distractors.reset()
-        self.primary.reset()
-        self.best.reset()
+    def lastgame(self, agent):
         
-    def won(self, index):
-        
-        if self.totalMatches == 0:
+        if len(agent.mines) <= 0 or len(agent.opponent) <= 0:
             return 0
         
-        res = self.mines[index] - self.opponent[index]
-        if res == 1 or res == -2:
+        res = (agent.mines[-1] - agent.opponent[-1]) % 3
+        if res == 1:
             return 1
-        elif res == -1 or res == 2:
+        elif res == 2:
             return -1
         return 0
         
+          
     def decide(self):
         
-        if self.totalMatches > 0 and self.won(-1) > 0:
-            self.totalWin = self.totalWin + 1
-        elif self.totalMatches > 0 and self.won(-1) < 0:
-            self.totalLoss = self.totalLoss + 1
+        for agent, scores in self.agents.items():
+            scores[0], scores[1] = (scores[0] - 1) / 1.05 + 1, (scores[1] - 1) / 1.05 + 1
             
-        self.totalMatches = self.totalMatches + 1
-            
-        ratio = (self.totalWin - self.totalLoss)
+            outcome = self.lastgame(agent)
+            if outcome > 0:
+                scores[0] += 3
+            elif outcome < 0:
+                scores[1] += 3
+            else:
+                scores[0], scores[1] = scores[0] + 3/2, scores[1] + 3/2
         
-        if ratio > self.delta:
-            self.executor = self.distractors
-        elif ratio <= self.delta and ratio > -self.delta:
-            self.executor = self.primary
-        else:
-            self.executor = self.best
         
-        return self.submit(self.executor.decide())
+        best_prob = -1
+        best_agent = None
+        best_move = None
+        for agent, scores in self.agents.items():
+            prob = np.random.beta(scores[0], scores[1])
+            move = agent.decide()
+            if prob > best_prob:
+                best_prob = prob
+                best_agent = agent
+                best_move = move
                 
+        self.executor = best_agent
                 
-        
- 
-       
-    
+        return best_move
     
 
-agents = [
-    rps.Randomer(),
-    rps.MirrorOpponentDecider(ahead = 0),
-    rps.MirrorSelfDecider(ahead = 0),
-    rps.MirrorOpponentDecider(ahead = 1),
-    rps.MirrorSelfDecider(ahead = 1),
-    rps.MirrorOpponentDecider(ahead = 2),
-    rps.MirrorSelfDecider(ahead = 2),
-    ]
+'''
+agency = Agency(agents)
 
-balancer = Balancer(agents, 3)
-primray = rps.Classifier(KNeighborsClassifier(), window = 10)
-best = rps.OClassifier(
-                rps.ClassifierHolder([
-                    AdaBoostClassifier(random_state = 17, n_estimators = 10),
-                    KNeighborsClassifier(),
-                    SVC(kernel = 'rbf')
-                ]), window = 10)
-
-distractor = Distractor(
-    primary = primray,
-    best = best,
-    distractors = balancer,
-    delta = 4
-    )
+for rnd in range(0, 1000):
+    t_start = time.perf_counter_ns()
+    choice = agency.decide()
+    t_end = time.perf_counter_ns()
+    agency.add(np.random.randint(0, 3))
+    print("Round {} Choice: {}, Elapse Time: {}".format(rnd + 1, choice, t_end - t_start))
 
 
-
-
-
+exit()
+'''
 
 '''
 OClassifier(XGBClassifier) window = 14
@@ -182,7 +126,7 @@ OClassifier(XGBClassifier) window = 14
 WIN : 0
 LOST: 0
 '''
-player1 = distractor
+player1 = Agency(agents)
 player2 = rps.Classifier(XGBClassifier(random_state = 17, n_estimators = 10, eval_metric = 'logloss'), 
                          window = 10)
 
@@ -196,8 +140,11 @@ win1 = 0
 win2 = 0
 
 for rnd in range(0, 1000):
-
+    
+    t_start = time.perf_counter_ns()
     move1 = player1.decide()
+    t_end = time.perf_counter_ns()
+    
     move2 = player2.decide()
     
     player1.add(move2)
@@ -224,8 +171,8 @@ for rnd in range(0, 1000):
         win2 = win2 + 1
 
 
-    msg = "[{:<4}]   {:>8} | {:<8} => {:^50}  {:>4}|{:<4}"
-    print(msg.format(rnd + 1, SIGNS[move1], SIGNS[move2], winner.__str__(), win1, win2))
+    msg = "[{:<4}]   {:>8} | {:<8} => {:>50}  {:>4}|{:<4} {}"
+    print(msg.format(rnd + 1, SIGNS[move1], SIGNS[move2], winner.__str__(), win1, win2, t_end - t_start))
 
 
 print("====================================================================")
