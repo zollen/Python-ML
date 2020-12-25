bf10883344b625a8447458027bb2c39c30f129fe
Stephen Kong <zollen.kong@gmail.com>
2020-12-25 16:14:21 -0500

    analysis other strong agent

--- rps_kaggle/others/rfind.py
+++ rps_kaggle/others/rfind.py
@@ -148,7 +148,85 @@ class Configuration:
 observation = Observation()
 configuration = Configuration()
 
+'''
 for rnd in range(0, 10):
     observation.step = rnd
     observation.lastOpponentAction = np.random.randint(0, 3)  
     print("Round [%d] MY PREDICTED MOVE: [%d]" %(rnd + 1, myagent(observation, configuration)))
+'''    
+
+
+import time
+from xgboost import XGBClassifier
+import rps_kaggle.lib.rps_lib as rps
+
+SIGNS = ['ROCK', 'PAPER', 'SCISSORS']
+
+class HighPerformer:
+    
+    def __str__(self):
+        return "HighPerformer"
+    
+    def add(self, rnd, move):
+        observation.step = rnd
+        observation.lastOpponentAction = move 
+      
+    def decide(self, observation, configuration):
+        return myagent(observation, configuration)
+        
+        
+player1 = HighPerformer()
+player2 = rps.Classifier(XGBClassifier(random_state = 17, n_estimators = 10, eval_metric = 'logloss'), 
+                         window = 10)
+results = []
+win1 = 0
+win2 = 0
+observation = Observation()
+configuration = Configuration()
+
+for rnd in range(0, 1000):
+    
+    t_start = time.perf_counter_ns()
+    move1 = player1.decide(observation, configuration)
+    t_end = time.perf_counter_ns()
+    
+    observation.step = rnd
+    move2 = player2.decide()
+    
+    player1.add(rnd, move2)
+    player2.add(move1)
+    
+    res = move1 - move2
+    
+    winner = "TIE"
+    
+    if res ==  1 or res == -1:
+        if res > 0:
+            winner = player1
+        else:
+            winner = player2
+    elif res == 2 or res == -2:
+        if res > 0:
+            winner = player2
+        else:
+            winner = player1
+            
+    if winner == player1:
+        win1 = win1 + 1
+    elif winner == player2:
+        win2 = win2 + 1
+
+
+    msg = "[{:<4}]   {:>8} | {:<8} => {:>50}  {:>4}|{:<4} {}"
+    print(msg.format(rnd + 1, SIGNS[move1], SIGNS[move2], winner.__str__(), win1, win2, t_end - t_start))
+
+
+print("====================================================================")
+if win1 == win2:
+    print("BOTH TIE!!")
+elif win1 > win2:
+    print("PLAYER1 {}, PLAYER2 {}  RATIO {:2.4f}".format(win1, win2, win1 / win2))
+    print("Player1: [{}] WON!!!!!".format(player1))
+else:
+    print("PLAYER1 {}, PLAYER2 {} RATIO {:2.4f}".format(win1, win2, win1 / win2))
+    print("Player1: [{}] LOST!!!!!".format(player1))
