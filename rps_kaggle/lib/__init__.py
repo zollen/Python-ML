d770d5ba50b26a2a0d5651ff11868404b40d8408
Stephen Kong <zollen.kong@gmail.com>
2021-01-13 13:05:37 -0500

    update library

--- rps_kaggle/lib/__init__.py
+++ rps_kaggle/lib/__init__.py
@@ -7,3 +7,4 @@ Created on Sep. 7, 2020
 from rps_kaggle.lib.rps_lib import *
 from rps_kaggle.lib.battle_lib import *
 from rps_kaggle.lib.enemy_lib import *
+from rps_kaggle.lib.enemy2_lib import *
