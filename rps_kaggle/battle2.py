af157deceb39b6b3e9f030f859d66437e317156a
Stephen Kong <zollen.kong@gmail.com>
2021-01-13 13:20:48 -0500

    testing 3rd party agents

--- rps_kaggle/battle2.py
+++ rps_kaggle/battle2.py
@@ -12,6 +12,8 @@ from sklearn.neighbors import KNeighborsClassifier
 import rps_kaggle.lib.rps_lib as rps
 import rps_kaggle.lib.battle_lib as bat
 import rps_kaggle.lib.enemy_lib as enm
+import rps_kaggle.lib.enemy2_lib as enm2
+import rps_kaggle.lib.enemy3_lib as enm3
 import warnings
 
 
@@ -31,17 +33,21 @@ def setup():
     xgb2 = rps.Sharer(xgb1, ahead = 1)
     xgb3 = rps.Sharer(xgb1, ahead = 2)
     managers = [
-                    [ XGBClassifier(n_estimators = 10, eval_metric = 'logloss'), [0, 0], [0] ],
-                    [ RandomForestClassifier(n_estimators = 10),                 [0, 0], [0] ],
-                    [ AdaBoostClassifier(n_estimators = 10),                     [0, 0], [0] ],
-                    [ SVC(kernel = 'rbf'),                                       [0, 0], [0] ]
-                ]
-    
+                 [ XGBClassifier(n_estimators = 10, eval_metric = 'logloss'), [0, 0], [0] ],
+                 [ RandomForestClassifier(n_estimators = 10),                 [0, 0], [0] ],
+                 [ KNeighborsClassifier(),                                    [0, 0], [0] ],
+                 [ SVC(kernel = 'rbf'),                                       [0, 0], [0] ]
+             ]
+
     agents = [ xgb1, xgb2, xgb3 ]
-        
+     
     player1 = rps.MetaAgency(managers, agents, window = 20, history = 50, random_threshold = -10, randomness = 0.1)
+
+#    player2 = enm.MutliArmAgent()
+#    player2 = enm2.MarkovChain(2, 0.9)
+    player2 = enm3.Iocaine()
+
    
-    player2 = enm.MutliArmAgent()
     
     return player1, player2
 
