7eef10f71d39242f51424199d0ca93869cb5bb7e
Stephen Kong <zollen.kong@gmail.com>
2021-01-14 17:58:04 -0500

    code updated

--- rps_kaggle/battle2.py
+++ rps_kaggle/battle2.py
@@ -29,31 +29,32 @@ PLAYER1 336, PLAYER2 335  RATIO 1.0030
 '''
 
 def setup():
-   
+    
     xgb1 = rps.Classifier(XGBClassifier(n_estimators = 10, eval_metric = 'logloss'), window = 15)
     xgb2 = rps.Sharer(xgb1, ahead = 1)
     xgb3 = rps.Sharer(xgb1, ahead = 2)
     managers = [
-                 [ XGBClassifier(n_estimators = 10, eval_metric = 'logloss'), [0, 0], [0] ],
-                 [ RandomForestClassifier(n_estimators = 10),                 [0, 0], [0] ],
-                 [ KNeighborsClassifier(),                                    [0, 0], [0] ],
-                 [ SVC(kernel = 'rbf'),                                       [0, 0], [0] ]
-             ]
-
+                     [ XGBClassifier(n_estimators = 10, eval_metric = 'logloss'), [0, 0], [0] ],
+                     [ RandomForestClassifier(n_estimators = 10),                 [0, 0], [0] ],
+                     [ KNeighborsClassifier(),                                    [0, 0], [0] ],
+                     [ SVC(kernel = 'rbf'),                                       [0, 0], [0] ]
+                 ]
+    
     agents = [ xgb1, xgb2, xgb3 ]
-     
+         
     player1 = rps.MetaAgency(managers, agents, window = 20, history = 50, random_threshold = -10, randomness = 0.1)
 
+   
+   
     if False:
-        player2 = enm.MutliArmAgent()
-    if False:
-        player2 = enm2.MarkovChain(2, 0.9)
+        player1 = enm.MutliArmAgent()
     if True:
-        player2 = enm2.MarkovChain2(2, 0.9)
+        player2 = enm2.MarkovChain(4, 0.9)
     if False:
         player2 = enm3.Iocaine()
     if False:
-        player2 = enm4.Iocaine2(num_predictor = 60)
+        player2 = enm4.Iocaine2(num_predictor = 100)
+
 
    
     
