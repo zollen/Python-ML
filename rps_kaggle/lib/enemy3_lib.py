36dfc8ea5c53b71b5b05595c3b5c58a30b94375c
Stephen Kong <zollen.kong@gmail.com>
2021-01-16 14:03:24 -0500

    memory pattern

--- rps_kaggle/lib/enemy3_lib.py
+++ rps_kaggle/lib/enemy3_lib.py
@@ -6,170 +6,144 @@ Created on Jan. 13, 2021
 
 
 import random
+import numpy as np 
+from typing import List, Dict, Tuple
+from operator import itemgetter
+from collections import defaultdict
 
 
-def recall(age, hist):
-    """Looking at the last 'age' points in 'hist', finds the
-    last point with the longest similarity to the current point,
-    returning 0 if none found."""
-    end, length = 0, 0
-    for past in range(1, min(age + 1, len(hist) - 1)):
-        if length >= len(hist) - past: break
-        for i in range(-1 - length, 0):
-            if hist[i - past] != hist[i]: break
-        else:
-            for length in range(length + 1, len(hist) - past):
-                if hist[-past - length - 1] != hist[-length - 1]: break
-            else: length += 1
-            end = len(hist) - past
-    return end
-
-def beat(i):
-    return (i + 1) % 3
-def loseto(i):
-    return (i - 1) % 3
-
-class Stats:
-    """Maintains three running counts and returns the highest count based
-         on any given time horizon and threshold."""
-    def __init__(self):
-        self.sum = [[0, 0, 0]]
-    def add(self, move, score):
-        self.sum[-1][move] += score
-    def advance(self):
-        self.sum.append(self.sum[-1])
-    def max(self, age, default, score):
-        if age >= len(self.sum): diff = self.sum[-1]
-        else: diff = [self.sum[-1][i] - self.sum[-1 - age][i] for i in range(3)]
-        m = max(diff)
-        if m > score: return diff.index(m), m
-        return default, score
-
-class Predictor:
-    """The basic iocaine second- and triple-guesser.    Maintains stats on the
-         past benefits of trusting or second- or triple-guessing a given strategy,
-         and returns the prediction of that strategy (or the second- or triple-
-         guess) if past stats are deviating from zero farther than the supplied
-         "best" guess so far."""
-    def __init__(self):
-        self.stats = Stats()
-        self.lastguess = -1
-    def addguess(self, lastmove, guess):
-        if lastmove != -1:
-            diff = (lastmove - self.prediction) % 3
-            self.stats.add(beat(diff), 1)
-            self.stats.add(loseto(diff), -1)
-            self.stats.advance()
-        self.prediction = guess
-    def bestguess(self, age, best):
-        bestdiff = self.stats.max(age, (best[0] - self.prediction) % 3, best[1])
-        return (bestdiff[0] + self.prediction) % 3, bestdiff[1]
-
-ages = [1000, 100, 10, 5, 2, 1]
-
-class Iocaine:
-
-    def __init__(self):
-        """Build second-guessers for 50 strategies: 36 history-based strategies,
-             12 simple frequency-based strategies, the constant-move strategy, and
-             the basic random-number-generator strategy.    Also build 6 meta second
-             guessers to evaluate 6 different time horizons on which to score
-             the 50 strategies' second-guesses."""
-        self.predictors = []
-        self.predict_history = self.predictor((len(ages), 2, 3))
-        self.predict_frequency = self.predictor((len(ages), 2))
-        self.predict_fixed = self.predictor()
-        self.predict_random = self.predictor()
-        self.predict_meta = [Predictor() for a in range(len(ages))]
-        self.stats = [Stats() for i in range(2)]
-        self.histories = [[], [], []]
-        self.opponent = None
-
-    def predictor(self, dims=None):
-        """Returns a nested array of predictor objects, of the given dimensions."""
-        if dims: return [self.predictor(dims[1:]) for i in range(dims[0])]
-        self.predictors.append(Predictor())
-        return self.predictors[-1]
+class MemoryPatterns:
     
-    def add(self, token):
-        self.opponent = token
+    def __init__(self, states=3, min_memory=2, max_memory=20, threshold=0.5, warmup=5, verbose=False):
+        self.min_memory = min_memory
+        self.max_memory = max_memory
+        self.threshold  = threshold
+        self.warmup     = warmup
+        self.verbose    = verbose
+        self.states     = states
+        self.step       = 0
+        self.history = {
+            "step":      [],
+            "reward":    [],
+            "opponent":  [],
+            "pattern":   [],
+            "action":    []
+        }
+        pass
+    
+    def __call__(self):
+        return self.agent()
+    
+    def add(self, lastOpponentAction):
+        self.update_state(lastOpponentAction)
+
+    def batch(self, iterable, n=1):
+        l = len(iterable)
+        for ndx in range(0, l, n):
+            yield iterable[ndx:min(ndx + n, l)]
+
+    def agent(self):
         
-    def __str__(self):
-        return "Iocaine()"
+        if self.step < self.warmup:
+            expected = self.random_action()
+        else:
+            for keys in [ ("opponent", "action"), ("opponent",) ]:
+                # history  = self.generate_history(["opponent", "action"])  # "action" must be last
+                history  = self.generate_history(["opponent"])  
+                memories = self.build_memory(history) 
+                patterns = self.find_patterns(history, memories)
+                if len(patterns): break
+            score, expected, pattern = self.find_best_pattern(patterns)
+            self.history['pattern'].append(pattern)    
+            if self.verbose:
+                print('keys    ', keys)
+                print('history ', history)
+                print('memories', memories)
+                print('patterns', patterns)
+                print('score   ', score)
+                print('expected', expected)
+                print('pattern ', pattern)
+
+        action = (expected + 1) % self.states
+        self.history['action'].append(action)
         
-    def decide(self):
-        if self.opponent == None:
-            return self.move(-1)          
-        return self.move(self.opponent)
-
-    def move(self, them):
-        """The main iocaine "move" function."""
-
-        # histories[0] stores our moves (last one already previously decided);
-        # histories[1] stores their moves (last one just now being supplied to us);
-        # histories[2] stores pairs of our and their last moves.
-        # stats[0] and stats[1] are running counters our recent moves and theirs.
-        if them != -1:
-            self.histories[1].append(them)
-            self.histories[2].append((self.histories[0][-1], them))
-            for watch in range(2):
-                self.stats[watch].add(self.histories[watch][-1], 1)
-
-        # Execute the basic RNG strategy and the fixed-move strategy.
-        rand = random.randrange(3)
-        self.predict_random.addguess(them, rand)
-        self.predict_fixed.addguess(them, 0)
-
-        # Execute the history and frequency stratgies.
-        for a, age in enumerate(ages):
-            # For each time window, there are three ways to recall a similar time:
-            # (0) by history of my moves; (1) their moves; or (2) pairs of moves.
-            # Set "best" to these three timeframes (zero if no matching time).
-            best = [recall(age, hist) for hist in self.histories]
-            for mimic in range(2):
-                # For each similar historical moment, there are two ways to anticipate
-                # the future: by mimicing what their move was; or mimicing what my
-                # move was.    If there were no similar moments, just move randomly.
-                for watch, when in enumerate(best):
-                    if not when: move = rand
-                    else: move = self.histories[mimic][when]
-                    self.predict_history[a][mimic][watch].addguess(them, move)
-                # Also we can anticipate the future by expecting it to be the same
-                # as the most frequent past (either counting their moves or my moves).
-                mostfreq, score = self.stats[mimic].max(age, rand, -1)
-                self.predict_frequency[a][mimic].addguess(them, mostfreq)
-
-        # All the predictors have been updated, but we have not yet scored them
-        # and chosen a winner for this round.    There are several timeframes
-        # on which we can score second-guessing, and we don't know timeframe will
-        # do best.    So score all 50 predictors on all 6 timeframes, and record
-        # the best 6 predictions in meta predictors, one for each timeframe.
-        for meta, age in enumerate(ages):
-            best = (-1, -1)
-            for predictor in self.predictors:
-                best = predictor.bestguess(age, best)
-            self.predict_meta[meta].addguess(them, best[0])
-
-        # Finally choose the best meta prediction from the final six, scoring
-        # these against each other on the whole-game timeframe. 
-        best = (-1, -1)
-        for meta in range(len(ages)):
-            best = self.predict_meta[meta].bestguess(len(self.histories[0]) , best) 
-
-        # We've picked a next move.    Record our move in histories[0] for next time.
-        self.histories[0].append(best[0])
-
-        # And return it.
-        return best[0]
-
-iocaine = None
-
-def iocaine_agent(observation, configuration):
-    global iocaine
-    if observation.step == 0:
-        iocaine = Iocaine()
-        act = iocaine.move(-1)
-    else:
-        act = iocaine.move(observation.lastOpponentAction)
+        if self.verbose:
+            print('action', action)
+        self.step += 1
+        return int(action) 
+       
+    def random_action(self) -> int:
+        return random.randint(0, self.states-1)
+  
+    def reward(self):
+        if len(self.history['action']) <= 0 or len(self.history['opponent']) <= 0:
+            return 0
         
-    return act
+        res = self.history['action'][-1] - self.history['opponent'][-1]
+        if res == 1:
+            return 1
+        elif res == 2:
+            return -1
+        return 0
+
+    def update_state(self, lastOpponentAction):
+        self.history['step'].append( self.step )
+        self.history['reward'].append( self.reward() )
+        if self.step != 0:
+            self.history['opponent'].append( lastOpponentAction )
+
+    def generate_history(self, keys: List[str]) -> List[Tuple[int]]:
+        history = list(zip(*[ reversed(self.history[key]) for key in keys ]))
+        history = list(reversed(history))
+        return history 
+    
+    def build_memory(self, history: List[Tuple[int]]) -> List[ Dict[Tuple[int], List[int]] ]:
+        output    = [ dict() ] * self.min_memory
+        expecteds = self.generate_history(["opponent"])
+        for batch_size in range(self.min_memory, self.max_memory+1):
+            if batch_size >= len(history): break  # ignore batch sizes larger than history
+            output_batch    = defaultdict(lambda: [0,0,0])
+            history_batches  = list(self.batch(history, batch_size+1))
+            expected_batches = list(self.batch(expecteds, batch_size+1))
+            for _, (pattern, expected_batch) in enumerate(zip(history_batches, expected_batches)):
+                previous_pattern = tuple(pattern[:-1])
+                expected         = (expected_batch[-1][-1] or 0) % self.states  # assume "action" is always last 
+                output_batch[ previous_pattern ][ expected ] += 1
+            output.append( dict(output_batch) )
+        return output
+
+    
+    def find_patterns(self, history: List[Tuple[int]], memories: List[ Dict[Tuple[int], List[int]] ]) -> List[Tuple[float, int, Tuple[int]]]:
+        patterns = []
+        for n in range(1, self.max_memory+1):
+            if n >= len(history): break
+                
+            pattern = tuple(history[-n:])
+            if pattern in memories[n]:
+                score    = np.std(memories[n][pattern])
+                expected = np.argmax(memories[n][pattern])
+                patterns.append( (score, expected, pattern) )
+        patterns = sorted(patterns, key=itemgetter(0), reverse=True)
+        return patterns
+    
+    
+    def find_best_pattern(self, patterns: List[Tuple[float, int, Tuple[int]]] ) -> Tuple[float, int, Tuple[int]]:
+        patterns       = sorted(patterns, key=itemgetter(0), reverse=True)
+        for (score, expected, pattern) in patterns:
+            break
+        else:
+            score    = 0.0
+            expected = self.random_action()
+            pattern  = tuple()
+        return score, expected, pattern
+    
+    
+    def get_pattern_scores(self):
+        pattern_rewards = defaultdict(list)
+        for reward, pattern in self.generate_history(["reward", "pattern"]):
+            pattern_rewards[pattern].append( reward )
+        pattern_scores = { pattern: np.mean(rewards) for pattern, rewards in pattern_rewards.items() }
+        return pattern_scores
+                    
+            
+            
