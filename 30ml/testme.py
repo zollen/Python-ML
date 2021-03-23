04382aeec55207f34082ccf678624ac0c0ba2086
Stephen Kong <zollen.kong@gmail.com>
2021-03-23 04:59:30 -0400

    increase test size

--- 30ml/testme.py
+++ 30ml/testme.py
@@ -21,7 +21,7 @@ train_df = pd.read_csv('../data/iris.csv')
 feature_df = train_df[['sepal.length', 'sepal.width', 'petal.length', 'petal.width']]
 label_df = train_df['variety']
 
-X_train, X_test, y_train, y_test = train_test_split(feature_df, label_df, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(feature_df, label_df, test_size=0.3, random_state=0)
 
 
 clf = LazyClassifier(verbose=0,ignore_warnings=True)
