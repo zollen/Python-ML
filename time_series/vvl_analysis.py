14168bd3e93c270151c879174b005bb854920a9e
Stephen Kong <zollen.kong@gmail.com>
2021-05-16 09:55:36 -0400

    work in progress

--- time_series/vvl_analysis.py
+++ time_series/vvl_analysis.py
@@ -28,7 +28,7 @@ pd.set_option('max_rows', None)
 
 SHOW_GRAPHS = False
 WEEKS_FOR_ANALYSIS = 24
-TEST_SIZE = (WEEKS_FOR_ANALYSIS * 7 * 0.1)
+TEST_SIZE = int(WEEKS_FOR_ANALYSIS * 7 * 0.1)
 TICKER = 'VVL.TO'
 
 
@@ -125,15 +125,47 @@ analysisNode = node(analysis_stock, inputs=["trade_data", "normalize_data"], out
 
 def train_archmodel(orig_data, normalize_data):
     
-    model = arch_model(orig_data, p=12, q=12)
-    model_fit = model.fit()
+    '''
+                     coef    std err          t      P>|t|       95.0% Conf. Int.
+    -----------------------------------------------------------------------------
+    omega          0.0258  9.879e-03      2.609  9.083e-03  [6.411e-03,4.514e-02]
+    alpha[1]       0.9532  4.340e-02     21.961 6.743e-107      [  0.868,  1.038]
+
+    model = arch_model(orig_data, p=1, q=0)
+    model_fit = model.fit(disp='off')
     
     print(model_fit.summary())
     
-    model = arch_model(normalize_data, p=12, q=12)
-    model_fit = model.fit()
+    
+                     coef    std err          t      P>|t|       95.0% Conf. Int.
+    -----------------------------------------------------------------------------
+    alpha[7]       0.1569  4.192e-02      3.743  1.819e-04    [7.475e-02,  0.239]
+    beta[7]        0.7827  7.511e-02     10.420  1.999e-25      [  0.635,  0.930]
+    
+    model = arch_model(normalize_data, p=7, q=7)
+    model_fit = model.fit(disp='off')
     
     print(model_fit.summary())
+    '''
+    
+    rolling_predictions1 = []
+    for i in range(TEST_SIZE):
+        train = orig_data[:-(TEST_SIZE-i)]
+        model = arch_model(train, p=1, q=0)
+        model_fit = model.fit(disp='off')
+        pred = model_fit.forecast(horizon=1)
+        rolling_predictions1.append(np.sqrt(pred.variance.values[-1,:][0]))
+        
+        
+    rolling_predictions2 = []
+    for i in range(TEST_SIZE):
+        train = normalize_data[:-(TEST_SIZE-i)]
+        model = arch_model(train, p=7, q=7)
+        model_fit = model.fit(disp='off')
+        pred = model_fit.forecast(horizon=1)
+        rolling_predictions2.append(np.sqrt(pred.variance.values[-1,:][0]))
+    
+    
 
 archModelNode = node(train_archmodel, inputs=["trade_data", "normalize_data"], outputs=None)
 
