77bf081e265626cec64c99f1de7660208809da8c
Stephen Kong <zollen.kong@gmail.com>
2021-06-02 17:08:25 -0400

    fix bug

--- sktime/tbats_exercise.py
+++ sktime/tbats_exercise.py
@@ -12,6 +12,7 @@ from tbats import TBATS
 from sklearn.metrics import mean_squared_error
 from sktime.datasets import load_airline
 from sktime.utils.plotting import plot_series
+from sktime.forecasting.model_selection import temporal_train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sb
 import warnings
@@ -20,19 +21,48 @@ warnings.filterwarnings('ignore')
 
 sb.set_style('whitegrid')
 
-train_df = pd.read_csv('train.csv')
-train_df = train_df[(train_df['store'] == 1) & (train_df['item'] == 1)] # item 1 in store 1
-
-train_df = train_df.set_index('date')
-y = train_df['sales']
-y_to_train = y.iloc[:(len(y)-365)]
-y_to_test = y.iloc[(len(y)-365):] 
-
-y_forecast = None
 if __name__ == '__main__':  
-    estimator = TBATS(seasonal_periods=(7, 365.25))
+    y = load_airline()
+    #plot_series(y);
+    y_to_train, y_to_test = temporal_train_test_split(y, test_size=36)
+    #plot_series(y_to_train, y_to_test, labels=["y_train", "y_test"])
+    #plt.show()
+
+if False:
+    yy = pd.DataFrame({'Date': y.index.to_timestamp(freq='M'), 'Price': y.values})
+    yy = yy.set_index('Date')
+    yy['Price'] = yy['Price'].astype('float64')
+    yy = yy.asfreq(pd.infer_freq(yy.index), method="pad")
+    stl = STL(yy)
+    result = stl.fit()
+    
+    seasonal, trend, resid = result.seasonal, result.trend, result.resid
+    plt.figure(figsize=(8,6))
+    
+    plt.subplot(4,1,1)
+    plt.plot(yy)
+    plt.title('Original Series', fontsize=16)
+    
+    plt.subplot(4,1,2)
+    plt.plot(trend)
+    plt.title('Trend', fontsize=16)
+    
+    plt.subplot(4,1,3)
+    plt.plot(seasonal)
+    plt.title('Seasonal', fontsize=16)
+    
+    plt.subplot(4,1,4)
+    plt.plot(resid)
+    plt.title('Residual', fontsize=16)
+    
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':  
+    estimator = TBATS(seasonal_periods=(12, 30.5))
     model = estimator.fit(y_to_train)
-    y_forecast = model.forecast(steps=365)
+    y_forecast = model.forecast(steps=36)
     print("=========================")
     print(model.summary())
     print("=========================")
@@ -49,14 +79,7 @@ if __name__ == '__main__':
     print(model.params.components.seasonal_harmonics)
     
     print("RMSE: %0.4f" % np.sqrt(mean_squared_error(y_to_test, y_forecast)))
-    if False:
-        plt.figure(figsize=(16,8))
-        plt.plot(y_to_test)
-        plt.plot(y_to_test.index, y_forecast)
-        plt.legend(('Data', 'Predictions'), fontsize=16)
-        plt.title("Price vs Prediction", fontsize=20)
-        plt.ylabel('Price', fontsize=16) 
-        plt.show()
-    
+    plot_series(y_to_train, y_to_test, pd.Series(data=y_forecast, index=y_to_test.index), labels=["y_train", "y_test", "y_pred"])
+    plt.show()
     
 print(datetime.now().strftime("%H:%M:%S"))
