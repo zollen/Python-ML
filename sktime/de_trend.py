8c2e3fed9be3b60f0f8f32bec13613b194a9296d
Stephen Kong <zollen.kong@gmail.com>
2021-06-04 15:17:10 -0400

    improve detrend

--- sktime/de_trend.py
+++ sktime/de_trend.py
@@ -3,7 +3,7 @@ Created on Jun. 2, 2021
 
 @author: zollen
 '''
-
+from statsmodels.tsa.stattools import adfuller
 
 from sktime.forecasting.trend import PolynomialTrendForecaster
 from sktime.transformations.series.detrend import Detrender
@@ -20,17 +20,46 @@ warnings.filterwarnings('ignore')
 
 sb.set_style('whitegrid')
 
+def adfuller_test(series, signif=0.05, name='', verbose=False):
+    """Perform ADFuller to test for Stationarity of given series and print report"""
+    r = adfuller(series, autolag='AIC', regression="c")
+    output = {'test_statistic':round(r[0], 4), 'pvalue':round(r[1], 4), 'n_lags':round(r[2], 4), 'n_obs':r[3]}
+    p_value = output['pvalue'] 
+    def adjust(val, length= 6): return str(val).ljust(length)
+
+    # Print Summary
+    print(f'    Augmented Dickey-Fuller Test on "{name}"', "\n   ", '-'*47)
+    print(f' Null Hypothesis: Data has unit root. Non-Stationary.')
+    print(f' Significance Level    = {signif}')
+    print(f' Test Statistic        = {output["test_statistic"]}')
+    print(f' No. Lags Chosen       = {output["n_lags"]}')
+
+    for key,val in r[4].items():
+        print(f' Critical value {adjust(key)} = {round(val, 3)}')
+
+    if p_value <= signif:
+        print(f" => P-Value = {p_value}. Rejecting Null Hypothesis.")
+        print(f" => Series is Stationary.")
+    else:
+        print(f" => P-Value = {p_value}. Weak evidence to reject the Null Hypothesis.")
+        print(f" => Series is Non-Stationary.")   
 
 y_train = load_airline()
 print(y_train.head())
+adfuller_test(y_train)
 
-forecaster = PolynomialTrendForecaster(degree=1)
+print("=================DE-TREND =========================")
+
+forecaster = PolynomialTrendForecaster(degree=2)
 transformer = Detrender(forecaster=forecaster)
 yt = transformer.fit_transform(y_train)
 
+adfuller_test(yt)
+
+
 # internally, the Detrender uses the in-sample predictions
 # of the PolynomialTrendForecaster
-forecaster = PolynomialTrendForecaster(degree=1)
+forecaster = PolynomialTrendForecaster(degree=2)
 fh_ins = -np.arange(len(y_train))  # in-sample forecasting horizon
 print(fh_ins)
 
