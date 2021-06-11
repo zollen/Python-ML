83b939a634818be4343297e3bea6d040634a5a59
Stephen Kong <zollen.kong@gmail.com>
2021-06-11 10:45:32 -0400

    CPU vs GPU comparison performance

--- numba/testme2.py
+++ numba/testme2.py
@@ -4,13 +4,12 @@ Created on Jun. 11, 2021
 @author: zollen
 '''
 
-from numba import jit
 import numpy as np
 import time as time
 
 x = np.arange(100000000).reshape(10000, 10000)
 
-@jit(nopython=True) 
+
 def go_fast(a):
     trace = 0.0
     for i in range(a.shape[0]):
@@ -18,16 +17,11 @@ def go_fast(a):
         
     return a + trace
 
-# do not measure this, compilation time + run time
-start_time = time.time()
-n = go_fast(x)
-end_time = time.time()
-
 # measure this, only the runtime is being measured.
 start_time = time.time()
 n = go_fast(x)
 end_time = time.time()
 
-print("GPU computation Time: ", end_time - start_time)
+print("CPU computation Time: ", end_time - start_time)
 
 
