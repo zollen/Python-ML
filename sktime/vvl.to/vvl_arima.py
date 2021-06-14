106df3ddd5ca0567dcf978e7447a00a350d0dd25
Stephen Kong <zollen.kong@gmail.com>
2021-06-14 12:11:21 -0400

    add ARIMA with exog

--- sktime/vvl.to/vvl_arima.py
+++ sktime/vvl.to/vvl_arima.py
@@ -18,6 +18,7 @@ from statsmodels.tsa.seasonal import STL
 from datetime import datetime, timedelta
 import pandas_datareader.data as web
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
 import warnings
@@ -97,16 +98,77 @@ print(model.summary())
 
 y_forecast, y_forecast_int = model.predict(fh, return_pred_int=True, alpha=0.05)
 
-print("RMSE: %0.4f" % mean_absolute_percentage_error(y_to_test, y_forecast))
-plt.figure(figsize=(10,4))
-plt.plot(y.iloc[-128:])
-plt.plot(y_to_test.index, y_forecast)
-plt.legend(('Data', 'Predictions'), fontsize=16)
-plt.title("ARIMA(VVL.TO)", fontsize=20)
+rmse = mean_absolute_percentage_error(y_to_test, y_forecast)
+rmse = round(rmse, 6)
+print("RMSE: %0.4f" % rmse)
+
+figure, ax = plt.subplots(2, 1, figsize=(10, 8))
+plt.subplots_adjust(hspace=1)
+
+ax[0].plot(y.iloc[-128:])
+ax[0].plot(y_to_test.index, y_forecast)
+ax[0].legend(('Data', 'Predictions'), fontsize=16)
+ax[0].set_title(F"ARIMA(VVL.TO) {rmse}", fontsize=20)
 #plt.xticks(rotation=90)
-plt.ylabel('Price', fontsize=16) 
-plt.ylim(32, 40)
-plt.fill_between(
+ax[0].set_ylabel('Price', fontsize=16) 
+ax[0].set_ylim(32, 40)
+ax[0].fill_between(
+            y_to_test.index,
+            y_forecast_int["lower"],
+            y_forecast_int["upper"],
+            alpha=0.2,
+            color='b'
+        )
+
+
+
+
+
+def exog_create():
+    
+    exog = pd.DataFrame({'Date': y.index})
+    exog = exog.set_index(exog['Date'])
+    exog.drop(columns=['Date'], inplace=True)
+    
+    freqs = [20, 81, 86, 94, 101, 102, 107, 146, 524, 147]
+    coeffs = [1.5870266419444485, 0.3186192943552314, 0.6017395178798809, 1.5687491458100933, 1.8466539435276494, 0.36883709046035296, 1.929456217767924, 1.841477121316948, 0.874471206109826, 1.005621522861652]
+    cols = ['sin20', 'cos20', 'sin81', 'cos81', 'sin86', 'cos86',
+             'sin94', 'cos94', 'sin101', 'cos101', 'sin102', 'cos102', 
+             'sin107', 'cos107', 'sin146', 'cos146', 'sin524', 'cos524',
+             'sin147', 'cos147']
+    
+    for freq, coeff in zip(freqs, coeffs):
+        exog['sin' + str(freq)] = coeff * np.sin(freq * np.pi * exog.index.dayofyear / 365.25)
+        exog['cos' + str(freq)] = coeff * np.cos(freq * np.pi * exog.index.dayofyear / 365.25)     
+    
+    exog = exog[cols]
+    
+    exog_train = exog.loc[y_to_train.index]
+    exog_test = exog.loc[y_to_test.index]
+    
+    return exog_train, exog_test
+    
+
+exog_train, exog_test = exog_create()
+
+model = ARIMA(order=(2, 1, 2), seasonal_order=(0, 1, 0, 8), suppress_warnings=True)
+kk = model.fit(y_to_train['Prices'], X=exog_train)
+print(model.summary())
+
+y_forecast, y_forecast_int = model.predict(fh, X=exog_test, return_pred_int=True, alpha=0.05)
+
+rmse = mean_absolute_percentage_error(y_to_test, y_forecast)
+rmse = round(rmse, 6)
+print("RMSE: %0.4f" % rmse)
+
+ax[1].plot(y.iloc[-128:])
+ax[1].plot(y_to_test.index, y_forecast)
+ax[1].legend(('Data', 'Predictions'), fontsize=16)
+ax[1].set_title(F"ARIMA(VVL.TO) {rmse}", fontsize=20)
+#plt.xticks(rotation=90)
+ax[1].set_ylabel('Price', fontsize=16) 
+ax[1].set_ylim(32, 40)
+ax[1].fill_between(
             y_to_test.index,
             y_forecast_int["lower"],
             y_forecast_int["upper"],
