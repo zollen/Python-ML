edf48dd5db418582fe0fb4dbcde57fac7f7962d4
Stephen Kong <zollen.kong@gmail.com>
2021-07-01 13:00:58 -0400

    use numpy to speed up processing

--- futuresales_kaggle/analysis/explore.py
+++ futuresales_kaggle/analysis/explore.py
@@ -4,17 +4,12 @@ Created on Jun. 27, 2021
 @author: zollen
 '''
 
-from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 import calendar
-import itertools
-import matplotlib.pyplot as plt
-import seaborn as sb
 import warnings
 
 warnings.filterwarnings('ignore')
-sb.set_style('whitegrid')
 
 pd.set_option('max_columns', None)
 pd.set_option('max_rows', None)
@@ -28,21 +23,7 @@ training = pd.read_csv('../data/sales_train.csv')
 testing = pd.read_csv('../data/test.csv')
 
 
-training['date'] = pd.to_datetime(training['date'], format="%d.%m.%Y")
-
-
-
-if False:
-    plt.figure(figsize=(10,4))
-    plt.xlim(-100, 3000)
-    flierprops = dict(marker='o', markerfacecolor='purple', markersize=6,
-                      linestyle='none', markeredgecolor='black')
-    sb.boxplot(x=training.item_cnt_day, flierprops=flierprops)
-    
-    plt.figure(figsize=(10,4))
-    plt.xlim(training.item_price.min(), training.item_price.max()*1.1)
-    sb.boxplot(x=training.item_price, flierprops=flierprops)
-    
+training['date'] = pd.to_datetime(training['date'], format="%d.%m.%Y")    
 
 training = training[(training.item_price < 300000 ) & (training.item_cnt_day < 1000)]
 
@@ -67,37 +48,38 @@ I need baseline evaluation!!!!!!!!!!!!!!
 2015-10-31 00:00:00
 '''
 
+'''
+there are lot more combos don't show up in test data
+Mismatch 307437  total:  418908
+26% of training combo are in the test data
+'''
 
-all_dates = [ '2012-12-31']
-for yr in [2013, 2014, 2015]:
-    for mn in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:        
+all_dates = [ ]
+for yr in  [ 2013, 2014, 2015 ]:
+    for mn in [1,2,3,4,5,6,7,8,9,10,11,12]:
         dd = calendar.monthrange(yr, mn)
         all_dates.append(f'{yr}-{mn:02d}-{dd[1]}')
         
 all_dates.remove('2015-11-30')
 all_dates.remove('2015-12-31')
 
-'''
-there are lot more combos don't show up in test data
-Mismatch 307437  total:  418908
-26% of training combo are in the test data
-'''
+
     
 def buildCombo(data, indx, rows):
      
     recRef = rows.iloc[0]
 
-    for i in range(len(all_dates) - 1):
+    for i in range(len(all_dates) + 1):
         
-        targets = rows[(rows['date'] > all_dates[i]) & (rows['date'] <= all_dates[i + 1])]   
+        targets = rows[rows['date_block_num'] == i]   
               
         rec = {}
-        rec['date'] = pd.to_datetime(all_dates[i + 1], format='%Y-%m-%d')
-        rec['date_block_num'] = recRef['date_block_num']
+        rec['date_block_num'] = i
         rec['shop_id'] = recRef['shop_id']
         rec['item_id'] = recRef['item_id']
         rec['item_price'] = recRef['item_price']
         
+
         if len(targets) <= 0:
             rec['item_cnt_day'] = 0
         else:
@@ -107,21 +89,50 @@ def buildCombo(data, indx, rows):
                 cnt = cnt + row['item_cnt_day'] 
             rec['item_cnt_day'] = cnt
             
-        data.loc[indx] = [ rec['date'], rec['date_block_num'],
+        data[indx] = [ 0, rec['date_block_num'],
                         rec['shop_id'], rec['item_id'], 
                         rec['item_price'], rec['item_cnt_day'] ] 
 
         indx += 1
-        
-        
-    
-    return indx, data
+         
+    return indx
     
 
-trainData = pd.DataFrame(columns=training.columns)
-index = 0
 '''
-buildData(trainData)
+TIME:
+targets = rows[(rows['date'] > all_dates[i]) & (rows['date'] <= all_dates[i + 1])]   
+TOTAL TIME:  7249.417484521866
+
+targets = rows[(rows['date_block_num'] == i)]  
+TOTAL TIME:  4570.942203760147
+'''
+import time
+
+nn = np.zeros((len(training) * len(all_dates), 6))
+index = 0
+
+def process(rows):
+    global nn, index
+    index = buildCombo(nn, index, rows)
+
+
+ts = time.time()   
+
+
+training.groupby(['shop_id', 'item_id']).apply(process)
+
+nn = nn[~np.all(nn == 0, axis = 1)]
+
+trainData = pd.DataFrame(data=nn, columns=training.columns)
+trainData['date_block_num'] = trainData['date_block_num'].astype('int64')
+
+print("TOTAL TIME: ", time.time() - ts)   
+
+
+def convDate(date):
+    return pd.to_datetime(all_dates[date], format='%Y-%m-%d')
+    
+trainData['date'] = trainData['date_block_num'].apply(convDate) 
 
 trainData['date'] = trainData['date'].astype('datetime64')
 trainData['date_block_num'] = trainData['date_block_num'].astype('int64')
@@ -130,17 +141,11 @@ trainData['item_id'] = trainData['item_id'].astype('int64')
 trainData['item_price'] = trainData['item_price'].astype('float64')
 trainData['item_cnt_day'] = trainData['item_cnt_day'].astype('int64')
 
-trainData.to_csv('../data/train_data.csv', index = False)
-'''
-def func(rows):
-    global trainData, index
-    index, trainData = buildCombo(trainData, index, rows)
-    
-training.groupby(['shop_id', 'item_id']).apply(func)
 print(trainData)
 
-   
+trainData.to_csv('../data/train_data.csv', index = False)
+
 print("Done")
 
-plt.show()
+
 
