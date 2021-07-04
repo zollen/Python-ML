41cecd3d2c56d69a011502ab9b69730099a9416f
Stephen Kong <zollen.kong@gmail.com>
2021-07-03 20:48:35 -0400

    work in progress

--- futuresales_kaggle/analysis/work1.py
+++ futuresales_kaggle/analysis/work1.py
@@ -6,20 +6,38 @@ Created on Jul. 2, 2021
 
 import pandas as pd
 import numpy as np
-from datetime import datetime
-from matplotlib import pyplot as plt
-import seaborn as sb
+import time
+from xgboost import XGBRegressor
 import warnings
 
 warnings.filterwarnings('ignore')
 
 pd.set_option('max_columns', None)
 pd.set_option('max_rows', None)
+pd.set_option('display.width', 1000)
 
 np.random.seed(0)
 
+features = ['date_block_num', 'shop_id', 'item_id', 'item_price']
+label = 'item_cnt_day'
+
 sales = pd.read_csv('../data/monthly_train.csv')
+test = pd.read_csv('../data/monthly_test.csv')
 
-sales['date'] = pd.to_datetime(sales['date'], format='%Y-%m-%d')
 
-print(sales.head(1000))
+ts = time.time()
+
+model = XGBRegressor()
+model.fit(sales[features], sales[label])
+preds = model.predict(test[features])
+
+test['item_cnt_month'] = preds
+test['item_cnt_month'] = test['item_cnt_month'].astype('int64')
+test[['ID', label]].to_csv('../data/prediction.csv', index = False)
+
+print("TIME: ", time.time() - ts)
+
+print("Done")
+
+
+
