448304bfb05c0a3d719188c61ea7cfdad80ff8bb
Stephen Kong <zollen.kong@gmail.com>
2021-07-09 17:00:33 -0400

    format code

--- futuresales_kaggle/analysis/check.py
+++ futuresales_kaggle/analysis/check.py
@@ -20,9 +20,6 @@ preds = pd.read_csv('../data/prediction.csv')
 train = pd.read_csv('../data/monthly_train.csv')
 
 
-kk = pd.merge(test, preds, how='inner')
-res = kk.loc[kk['item_cnt_month'] < 0, ['shop_id', 'item_id', 'item_cnt_month']]
-
 '''
 shop_id, item_id, date_block_num, lg2, lg1, item_cnt_month
 1              2          0                   1                   
@@ -33,21 +30,16 @@ shop_id, item_id, date_block_num, lg2, lg1, item_cnt_month
 1              2          5         1    2    2
 1              2          6         2    2    1
 
-
-start
-https://stackoverflow.com/questions/33907537/groupby-and-lag-all-columns-of-a-dataframe
-need more
-
 https://stackoverflow.com/questions/20410312/how-to-create-a-lagged-data-structure-using-pandas-dataframe
 https://rayheberer.medium.com/generating-lagged-pandas-columns-10397309ccaf    
 '''
-def lag_feature(df, trailing_window_size, columns, target):
+def lag_features(df, trailing_window_size, columns, targets):
     
     df_lagged = df.copy()
    
     for window in range(1, trailing_window_size + 1):
-        shifted = df[columns + [ target] ].groupby(columns).shift(window)
-        shifted.columns = [ target + "_lag" + str(window) ]
+        shifted = df[columns + targets ].groupby(columns).shift(window)
+        shifted.columns = [x + "_lag" + str(window) for x in df[targets]]
         df_lagged = pd.concat((df_lagged, shifted), axis=1)
     df_lagged.dropna(inplace=True)
     
@@ -57,7 +49,9 @@ def lag_feature(df, trailing_window_size, columns, target):
 keys = ['shop_id', 'item_id']
 
 tstart = time.time()
-t = lag_feature(train, 3, keys, 'item_cnt_month')
+t = lag_features(train, 3, keys, ['item_cnt_month'])
 tend = time.time()
 print(t.head(500))
 print("TIME: ", tend - tstart)
+
+
--- futuresales_kaggle/analysis/work1.py
+++ futuresales_kaggle/analysis/work1.py
@@ -37,6 +37,7 @@ def lag_features(df, trailing_window_size, columns, targets):
 
 label = 'item_cnt_month'
 
+raw = pd.read_csv('../data/sales_train.csv')
 train = pd.read_csv('../data/monthly_train.csv')
 test = pd.read_csv('../data/monthly_test.csv')
 items = pd.read_csv('../data/monthly_items.csv')
@@ -45,15 +46,44 @@ shops = pd.read_csv('../data/monthly_shops.csv')
 
 ts = time.time()
 
+
+'''
+merge cats, shops and items
+'''
 items_cats = pd.merge(items, cats, how='left', on='item_category_id')
 train_item_cats = pd.merge(train, items_cats, how='left', on='item_id')
 test_item_cats = pd.merge(test, items_cats, how='left', on='item_id')
 train_item_cats_shops = pd.merge(train_item_cats, shops, how='left', on='shop_id')
 test_item_cats_shops = pd.merge(test_item_cats, shops, how='left', on='shop_id')
 
+
+
+'''
+clip values between 0 and 20
+'''
 train_item_cats_shops[label] = train_item_cats_shops[label].clip(0, 20)
 
 
+
+'''
+adding new features
+'''
+
+# 1. date_item_avg_cnt (groupby(['date_block_num', 'item_id']).agg({'item_cnt_month': ['mean']})
+f1 = raw.groupby(['date_block_num', 'item_id']).agg({'item_cnt_day': ['mean']})
+f1.columns = [ 'date_item_avg_cnt' ]
+train_item_cats_shops = train_item_cats_shops.merge(f1, on=['date_block_num', 'item_id'], how='left')
+train_item_cats_shops.fillna(0, inplace = True)
+test_item_cats_shops = test_item_cats_shops.merge(f1, on=['date_block_num', 'item_id'], how='left')
+test_item_cats_shops.fillna(0, inplace = True)
+
+
+
+
+
+'''
+adding lag features
+'''
 all_df = pd.concat([train_item_cats_shops, test_item_cats_shops])
 all_df.drop(columns=['ID'], inplace=True)
 
@@ -61,21 +91,28 @@ features = ['date_block_num', 'shop_id', 'item_id',
             'shop_category', 'shop_city',
             'item_price', 'item_category_id', 'name2', 
             'name3', 'item_type', 'item_subtype',
-            'item_cnt_month_lag1', 'item_cnt_month_lag2', 
-            'item_cnt_month_lag3' ]
+            'item_cnt_month_lag1', 'item_cnt_month_lag2', 'item_cnt_month_lag3',
+            'date_item_avg_cnt_lag1', 'date_item_avg_cnt_lag2', 'date_item_avg_cnt_lag3'
+            ]
             
 keys = ['shop_id', 'item_id']
-targets = ['item_cnt_month' ]
+targets = ['item_cnt_month', 'date_item_avg_cnt' ]
 
 all_df.loc[all_df['date_block_num'] == 34, 'item_cnt_month'] = 0
 
-postprossed = lag_features(all_df, 3, keys, targets)
+pp = lag_features(all_df, 3, keys, targets)
+
+pp.drop(columns=['date_item_avg_cnt'], inplace = True)
 
 
-t1 = postprossed[postprossed['date_block_num'] < 34]
-t2 = postprossed.loc[postprossed['date_block_num'] == 34, 
-                                ['shop_id', 'item_id', 'item_cnt_month_lag1', 
-                                'item_cnt_month_lag2', 'item_cnt_month_lag3']]
+t1 = pp[pp['date_block_num'] < 34]
+t2 = pp.loc[pp['date_block_num'] == 34, 
+                [
+                    'shop_id', 'item_id', 'item_cnt_month_lag1', 
+                    'item_cnt_month_lag2', 'item_cnt_month_lag3',
+                    'date_item_avg_cnt_lag1', 'date_item_avg_cnt_lag2', 
+                    'date_item_avg_cnt_lag3'
+                ]]
 
 print(t1.head())
 
