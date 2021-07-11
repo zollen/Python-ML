7fd5202a204e33c496c083c589706723e41ef111
Stephen Kong <zollen.kong@gmail.com>
2021-07-11 13:05:37 -0400

    add more formula

--- sympy/testme1.py
+++ sympy/testme1.py
@@ -6,6 +6,8 @@ Created on Jul. 11, 2021
 
 from sympy import * 
 
+init_printing(use_unicode=True)
+
 print(sqrt(8))
 
 x, y = symbols('x y')
@@ -22,7 +24,6 @@ print(factor(ee))
 
 
 x, y, z, nu = symbols('x t z nu')
-init_printing(use_unicode=True)
 
 # Derivative f(x) = sin(x)
 print(diff(sin(x), x))
@@ -36,6 +37,9 @@ print(limit(sin(x)/x, x, 0))
 # solve x^2 - 2 = 0
 print(solve(x**2 - 2, x))
 
+expr = x**3 + 4 * x*y - z
+print(expr.subs([(x, 2), (y, 4), (z, 0)]))
+
 # solve the differential equstion y'' - y = e^t
 t = symbols('t')
 y = Function('y')
@@ -46,3 +50,30 @@ print(Matrix([[1,2],[2,2]]).eigenvals())
 
 # print out the latex block of integrate(cos(x)^2 dx
 print(latex(Integral(cos(x)**2, (x, 0, pi))))
+
+x, y, z = symbols('x y z')
+M = Matrix([
+            [1, 0, 1, 3],
+            [2, 3, 4, 7],
+            [-1, -3, -3, -4]
+            ])
+print(M)
+print(M.rref())
+print(solve_linear_system(M, x, y, z))
+print(shape(M))
+print(shape(M.T))
+
+M = Matrix([
+            [3, -2, 4 , -2],
+            [5,  3, -3, -2],
+            [5, -2, 2,  -2],
+            [5, -2, -3,  3]
+            ])
+print("=====================")
+print(M.det())
+print("=====================")
+print(M.eigenvals())
+print("====================")
+print(M.eigenvects())
+print("==========PDP^-1==========")
+print(M.diagonalize())
