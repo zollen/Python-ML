e64b0dff90458239a3a2d964d64f46192877e872
Stephen Kong <zollen.kong@gmail.com>
2021-07-11 13:16:12 -0400

    add namespace

--- sympy/testme1.py
+++ sympy/testme1.py
@@ -4,67 +4,67 @@ Created on Jul. 11, 2021
 @author: zollen
 '''
 
-from sympy import * 
+import sympy as sy
 
-init_printing(use_unicode=True)
+sy.init_printing(use_unicode=True)
 
-print(sqrt(8))
-print(sqrt(8).evalf())
+print(sy.sqrt(8))
+print(sy.sqrt(8).evalf())
 
-x, y = symbols('x y')
+x, y = sy.symbols('x y')
 expr = x + 2 * y
 print(expr)
 
 print(expr + 1)
 print(expr - x)
 
-ee = expand(x * expr)
+ee = sy.expand(x * expr)
 print(ee)
 
-print(factor(ee))
+print(sy.factor(ee))
 
 
-x, y, z, nu = symbols('x t z nu')
+x, y, z, nu = sy.symbols('x t z nu')
 
 # Derivative f(x) = sin(x)
-print(diff(sin(x), x))
+print(sy.diff(sy.sin(x), x))
 
 # integral f'(x) = -sin(x)
-print(integrate(-sin(x), x))
+print(sy.integrate(-sy.sin(x), x))
 
 # limit x-> 0 sin(x)/x
-print(limit(sin(x)/x, x, 0))
+print(sy.limit(sy.sin(x)/x, x, 0))
 
 # solve x^2 - 2 = 0
-print(solve(x**2 - 2, x))
+print(sy.solve(x**2 - 2, x))
 
 expr = x**3 + 4 * x*y - z
 print(expr.subs([(x, 2), (y, 4), (z, 0)]))
 
 # solve the differential equstion y'' - y = e^t
-t = symbols('t')
-y = Function('y')
-print(dsolve(Eq(y(t).diff(t, t) - y(t), exp(t)), y(t)))
+t = sy.symbols('t')
+y = sy.Function('y')
+print(sy.dsolve(sy.Eq(y(t).diff(t, t) - y(t), sy.exp(t)), y(t)))
 
 # find the eign values of [[1, 2], [2, 2]]
-print(Matrix([[1,2],[2,2]]).eigenvals())
+print(sy.Matrix([[1,2],[2,2]]).eigenvals())
 
 # print out the latex block of integrate(cos(x)^2 dx
-print(latex(Integral(cos(x)**2, (x, 0, pi))))
+print(sy.latex(sy.Integral(sy.cos(x)**2, (x, 0, sy.pi))))
 
-x, y, z = symbols('x y z')
-M = Matrix([
+x, y, z = sy.symbols('x y z')
+M = sy.Matrix([
             [1, 0, 1, 3],
             [2, 3, 4, 7],
             [-1, -3, -3, -4]
             ])
 print(M)
 print(M.rref())
-print(solve_linear_system(M, x, y, z))
-print(shape(M))
-print(shape(M.T))
+print(sy.solve_linear_system(M, x, y, z))
+print(sy.shape(M))
+print(sy.shape(M.T))
 
-M = Matrix([
+M = sy.Matrix([
             [3, -2, 4 , -2],
             [5,  3, -3, -2],
             [5, -2, 2,  -2],
