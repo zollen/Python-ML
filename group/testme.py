5a78d51e57dee19697c61d19be704d00fefcff09
Stephen Kong <zollen.kong@gmail.com>
2021-07-22 17:07:33 -0400

    pandas group exercise

---
+++ group/testme.py
@@ -0,0 +1,46 @@
+'''
+Created on Jul. 22, 2021
+
+@author: zollen
+'''
+
+import pandas as pd
+
+df = pd.DataFrame({
+        'key'   : [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        'month' : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        'prices': [0.1, 0.2, 0.1, 0.5, 0.0, 0.3, 0.2, 0.0, 0.3, 0.1, 0.2, 0.3, 0.2, 0.1, 0.6, 0.2, 0.1, 0.1, 0.0, 0.1],
+        'cnt'   : [ 0, 0, 1, 3, 1, 0, 0, 2, 0, 0, 0, 0, 1, 1, 0, 5, 0, 0, 0, 0 ] 
+        })
+
+
+
+def calculate(data):   
+    score = 0
+    lgth = data.size
+    total = lgth**2
+    for idx, row in zip(range(1, lgth + 1), data.values):
+        score += row[0] * idx**2 / total
+        
+    return score
+    
+
+grps = df.groupby('key')
+
+for grp in grps.groups:
+    members = grps.get_group(grp).sort_values('month')
+    print("Group: ", grp)
+    print(members)
+
+
+
+print("Number of groups: ", grps.ngroups)
+
+print("first group")
+print("===========")
+print(grps.first())
+print("last group")
+print("===========")
+print(grps.last())
+#print(calculate(df['cnt']))
+
