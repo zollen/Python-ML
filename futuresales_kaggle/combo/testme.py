d5378c0ce54fa29e539501220dacf2ace2b1477d
Stephen Kong <zollen.kong@gmail.com>
2022-03-10 16:37:28 -0500

    best score

---
+++ futuresales_kaggle/combo/testme.py
@@ -0,0 +1,35 @@
+'''
+Created on Mar. 10, 2022
+
+@author: zollen
+'''
+
+import pandas as pd
+import numpy as np
+import warnings
+
+
+warnings.filterwarnings('ignore')
+
+pd.set_option('max_columns', None)
+pd.set_option('max_rows', None)
+pd.set_option('display.width', 1000)
+
+xgb1 = pd.read_csv('xgb1.csv.data')
+xgb2 = pd.read_csv('xgb2.csv.data')
+xgb3 = pd.read_csv('xgb3.csv.data')
+lgbm1 = pd.read_csv('lgbm1.csv.data')
+cat1 = pd.read_csv('cat1.csv.data')
+
+df = pd.DataFrame()
+df['ID'] = xgb1['ID']
+df['item_cnt_month'] = (xgb1['item_cnt_month'] + xgb2['item_cnt_month'] + 
+                        xgb3['item_cnt_month'] + lgbm1['item_cnt_month']) / 4
+
+
+
+df.to_csv('../data/prediction.csv', index = False)
+
+
+
+print("DONE")
