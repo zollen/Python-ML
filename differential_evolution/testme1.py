404c1514bfc17152e56ebd09fa893791d5cb862e
Stephen Kong <zollen.kong@gmail.com>
2022-06-06 11:24:43 -0400

    differential evolution generic algo

---
+++ differential_evolution/testme1.py
@@ -0,0 +1,101 @@
+'''
+Created on Jun. 4, 2022
+
+@author: zollen
+@url: https://pymoo.org/problems/definition.html#nb-problem-definition-elementwise
+@url: https://towardsdatascience.com/pymoode-differential-evolution-in-python-78e4221e5cbe
+@desc: Differential evolution (DE) (Storn & Price, 1997) was originally designed for scalar objective 
+    optimization. However, because of its simple implementation and efficient problem-solving quality, 
+    DE has been modified in different ways to solve multi-objective optimization problems.
+    
+DE: Differential Evolution for single-objective problems proposed by Storn & Price (1997). 
+    Other features later implemented are also present, such as dither, jitter, selection variants, 
+    and crossover strategies. For details see Price et al. (2005).
+'''
+
+import numpy as np
+from pymoo.core.problem import Problem, ElementwiseProblem
+from pymoode.de import DE
+from pymoo.optimize import minimize
+from pymoo.util.termination.default import SingleObjectiveDefaultTermination
+
+#Defining the objective function
+def rastrigin(x):
+    return np.sum(x * x - 10 * np.cos(2 * np.pi * x)) + 10 * np.size(x)
+
+'''
+f(x) = A * n + Σ ( x(i) - A * cos(2 * π * x(i) )
+
+    A is a user-specified parameter (usually 10) and 
+    n is the number of decision variables used. In our implementation, 
+    we will use two decision variables. Both will be bounded by -5.12 and 5.12.
+'''
+
+# Object-oriented definition which implements a method evaluating a set of solutions.
+class ProblemF1(Problem):
+    def __init__(self):
+        xl = np.full(2, -5.12)
+        xu = np.full(2, 5.12)
+        super().__init__(
+            n_var=2, n_obj=1, n_constr=0,
+            xl=xl, xu=xu)
+    def _evaluate(self, x, out, *args, **kwargs):
+        # x is an array corresponding to the decision variables in a Population with shape (N, m) 
+        # N - population size, m - number of decision variables 
+        out["F"] = np.sum(x * x - 10 * np.cos(2 * np.pi * x), axis=1) + 10 * x.shape[1]
+        
+        
+# Object-oriented definition which implements a function evaluating a single solution at a time.        
+class ElementwiseF1(ElementwiseProblem):
+    def __init__(self):
+        xl = np.full(2, -5.12)
+        xu = np.full(2, 5.12)
+        super().__init__(
+            n_var=2, n_obj=1, n_constr=0,
+            xl=xl, xu=xu)
+    def _evaluate(self, x, out, *args, **kwargs):
+        # x is a single solution
+        out["F"] = rastrigin(x)
+'''
+ Therefore, I have used 30 as the population size N to ensure global convergence. The DE variant was 
+ selected as the most usual DE/rand/1/bin, the F parameter as (0.3, 1.0) which uses random uniform 
+ distribution dither, and the CR parameter as 0.5, to reinforce the search on the coordinate axis and 
+ control convergence.
+ 
+* pop_size (int, optional): Population size.
+* sampling (Sampling, Population, or array-like, optional): Sampling strategy of pymoo.
+* variant (str, optional): Differential evolution strategy. Must be a string in the format: 
+    “DE/selection/n/crossover”. Defaults to “DE/rand/1/bin”.
+* CR (float, optional): Crossover parameter. Defined in the range [0, 1]. 
+    To reinforce mutation, use higher values. To control convergence speed, use lower values.
+* F (iterable of float or float, optional): Scale factor or mutation parameter. 
+    Defined in the range (0, 2]. To reinforce exploration, use higher values; 
+    whereas for exploitation, use lower.
+* gamma (float, optional): Jitter deviation parameter. Should be in the range (0, 2). Defaults to 1e-4.
+* pm (Mutation, optional): Pymoo’s mutation operators after crossover. Defaults to None.
+* repair (callable or str): Repair strategy to mutant vectors outside problem boundaries. 
+    The strategies adopted are based on Price et al. (2005). Defaults to “bounce-back”.
+* survival (Survival, optional): Pymoo’s survival strategy. Should be considered in multi-objective algorithms.
+'''
+de = DE(pop_size=30, variant="DE/rand/1/bin", F=(0.3, 1.0), CR=0.5)
+
+termination_1 = SingleObjectiveDefaultTermination(
+    x_tol=1e-6,
+    cv_tol=0.0,
+    f_tol=1e-6,
+    nth_gen=5,
+    n_last=20,
+    n_max_gen=100)
+
+
+res = minimize(
+    ElementwiseF1(),
+    de,
+    termination=termination_1,
+    seed=12,
+    save_history=True,
+    verbose=True)
+
+
+print("BEST: ", res.X)
+print("VAL: ", res.F)
---
+++ differential_evolution/testme2.py
@@ -0,0 +1,82 @@
+'''
+Created on Jun. 5, 2022
+
+@author: zollen
+@author: zollen
+@url: https://pymoo.org/problems/definition.html#nb-problem-definition-elementwise
+@url: https://towardsdatascience.com/pymoode-differential-evolution-in-python-78e4221e5cbe
+@desc: Differential evolution (DE) (Storn & Price, 1997) was originally designed for scalar objective 
+    optimization. However, because of its simple implementation and efficient problem-solving quality, 
+    DE has been modified in different ways to solve multi-objective optimization problems.
+    
+NSDE: Non-dominated Sorting Differential Evolution, a multi-objective algorithm that combines 
+    DE mutation and crossover operators to NSGA-II (Deb et al., 2002) survival.
+GDE3: Generalized Differential Evolution 3, a multi-objective algorithm that combines DE mutation and 
+    crossover operators to NSGA-II survival with a hybrid type survival strategy. In this algorithm, 
+    individuals might be removed in a one-to-one comparison before truncating the population by the 
+    multi-objective survival operator. It was proposed by Kukkonen, S. & Lampinen, J. (2005).
+NSDE-R: Non-dominated Sorting Differential Evolution based on Reference directions 
+    (Reddy & Dulikravich, 2019). It is an algorithm for many-objective problems that works as an 
+    extension of NSDE using NSGA-III (Deb & Jain, 2014) survival strategy.
+'''
+
+import numpy as np
+from pymoo.core.problem import Problem
+from pymoode.gde3 import GDE3
+from pymoode.nsde import NSDE
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.optimize import minimize
+#Termination
+from pymoo.util.termination.default import MultiObjectiveDefaultTermination
+
+'''
+In this example, I will introduce two conflicting convex objectives with additional difficulty 
+    introduced by constraints. The problem will be defined over two decision variables x1 and x2, 
+    both bounded by -5.0 and 5.0.
+
+f1(x) = (x1 - 0.5)^2 + 0.7 * x1 * x2 + 1.2(x2 + 0.7)^2
+f2(x) = (x1 + 1.5)^2 + 0.8 * x1 * x2 + 1.3(x2 - 1.7)^2
+
+g1(x) = x1^2 + (x2 - 1)^2 - 9 <= 0
+g2(x) = (x1 + 0.5)^2 + (x2 - 1)^2 + 2 <= 0
+'''
+
+class ProblemF2(Problem):
+    def __init__(self):
+        xl = np.full(2, -5.0)
+        xu = np.full(2, 5.0)
+        super().__init__(n_var=2, n_obj=2, n_constr=2, xl=xl, xu=xu)
+        
+    def _evaluate(self, x, out, *args, **kwargs):
+        F1 = (x[:, 0] - 0.5) ** 2 + 0.7 * x[:, 0] * x[:, 1] + 1.2 * (x[:, 1] + 0.7) ** 2
+        F2 = 1.1 * (x[:, 0] + 1.5) ** 2 + 0.8 * x[:, 0] * x[:, 1] + 1.3 * (x[:, 1] - 1.7) ** 2
+        out["F"] = np.column_stack([F1, F2])
+        G1 = x[:, 0] ** 2 + (x[:, 1] - 1) ** 2 - 9
+        G2 = - (x[:, 0] + 0.5) ** 2 - (x[:, 1] - 1) ** 2 + 2
+        out["G"] = np.column_stack([G1, G2])
+        
+
+
+termination_multi = MultiObjectiveDefaultTermination(
+    x_tol=1e-8,
+    cv_tol=1e-8,
+    f_tol=1e-8,
+    nth_gen=5,
+    n_last=50,
+    n_max_gen=200)
+
+
+gde3 = GDE3(pop_size=50, variant="DE/rand/1/bin", F=(0.0, 1.0), CR=0.7)
+nsde = NSDE(pop_size=50, variant="DE/rand/1/bin", F=(0.0, 1.0), CR=0.7)
+nsga2 = NSGA2(pop_size=30)
+
+res = minimize(
+    ProblemF2(),
+    gde3,
+    termination_multi,
+    seed=12,
+    save_history=True,
+    verbose=True)
+
+print("============== BEST ============== ")
+print(res.X)
