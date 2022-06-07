e41ea838714e76477fe5d28910ea0551e2d8d143
Stephen Kong <zollen.kong@gmail.com>
2022-06-07 10:05:54 -0400

    code improvement

--- differential_evolution/testme3.py
+++ differential_evolution/testme3.py
@@ -15,12 +15,14 @@ NSDE-R: It combines the survival operator of NSGA-III with the reproduction oper
     It has improved a lot the performance of GDE3 in many-objective problems.
 '''
 
+import numpy as np
 from pymoode.nsder import NSDER
 from pymoo.algorithms.moo.nsga3 import NSGA3
 from pymoode.gde3 import GDE3
 from pymoo.optimize import minimize
 from pymoode.survivors import RankSurvival
 from pymoo.factory import get_reference_directions, get_problem
+from pymoo.factory import get_performance_indicator
 
 '''
 DTLZ2 sample problem
@@ -50,11 +52,8 @@ ref_dirs = get_reference_directions(
     "das-dennis", 3, n_partitions=15)
 #Suggestion for NSGA-III
 popsize = ref_dirs.shape[0] + ref_dirs.shape[0] % 4
-nsder = NSDER(ref_dirs, pop_size=popsize,
-    variant="DE/rand/1/bin", F=(0.0, 1.0), CR=0.5)
-gde3mnn = GDE3(pop_size=popsize,
-    variant="DE/rand/1/bin", F=(0.0, 1.0), CR=0.2,
-    survival=RankSurvival(crowding_func="mnn"))
+nsder = NSDER(ref_dirs, pop_size=popsize, variant="DE/rand/1/bin", F=(0.0, 1.0), CR=0.5)
+gde3 = GDE3(pop_size=50, variant="DE/rand/1/bin", F=(0.0, 1.0), CR=0.7)
 nsga3 = NSGA3(ref_dirs, pop_size=popsize)
 
 '''
@@ -70,6 +69,41 @@ res_nsder = minimize(
     ('n_gen', 250),
     seed=SEED,
     save_history=True,
-    verbose=True)
+    verbose=False)
 
+print("============== NSDE-R ============== ")
 print(res_nsder.X)
+
+res_dge3 = minimize(
+    get_problem("dtlz2"),
+    gde3,
+    ('n_gen', 250),
+    seed=SEED,
+    save_history=True,
+    verbose=False)
+
+print("============== GDE3 ============== ")
+print(res_dge3.X)
+
+res_nsga3 = minimize(
+    get_problem("dtlz2"),
+    nsga3,
+    ('n_gen', 250),
+    seed=SEED,
+    save_history=True,
+    verbose=False)
+
+print("============== GDE3-MNN ============== ")
+print(res_nsga3.X)
+
+objs_p2 = np.row_stack([res_nsder.F, res_dge3.F, res_nsga3.F])
+nadir_p2 = objs_p2.max(axis=0)
+reference_p2 = nadir_p2 + 1e-6
+ideal_p2 =  objs_p2.min(axis=0)
+hv_p2 = get_performance_indicator("hv", ref_point=reference_p2, zero_to_one=True,
+                                  nadir=nadir_p2, ideal=ideal_p2)
+
+print("============== PERFORMANCE ==============")
+print("hv NSDE-R", hv_p2.do(res_nsder.F))
+print("hv GDE3-MNN", hv_p2.do(res_dge3.F))
+print("hv NSGA-III", hv_p2.do(res_nsga3.F))
