6b95739cf224d69aa07c57ac582f67d3b26909c8
Stephen Kong <zollen.kong@gmail.com>
2024-03-05 17:05:02 -0500

    add symbolic regression

--- symbolic_regress/testme2.py
+++ symbolic_regress/testme2.py
@@ -12,17 +12,17 @@ from gplearn.genetic import SymbolicRegressor
 rng = check_random_state(0)
 
 # Training samples
-X_train = rng.uniform(-1, 1, 15000).reshape(5000, 3)
-y_train = np.sin(X_train[:, 0])**2 + np.cos(X_train[:, 1]) + np.tan(X_train[:, 2])
+X_train = rng.uniform(-10, 10, 15000).reshape(5000, 3)
+y_train = 2 * X_train[:, 0]**3 + 3 * X_train[:, 1]**2 + 4 * X_train[:, 2]
 
 # Testing samples
-X_test = rng.uniform(-1, 1, 15000).reshape(5000, 3)
-y_test = np.sin(X_train[:, 0])**2 + np.cos(X_train[:, 1]) + np.tan(X_train[:, 2])
+X_test = rng.uniform(-10, 10, 15000).reshape(5000, 3)
+y_test = 2 * X_train[:, 0]**3 + 3 * X_train[:, 1]**2 + 4 * X_train[:, 2]
 
 
-est_gp = SymbolicRegressor(population_size=5000,
-                           generations=30, stopping_criteria=0.01,
-                           function_set=['add', 'mul', 'sin', 'cos', 'tan'],
+est_gp = SymbolicRegressor(population_size=10000,
+                           generations=40, stopping_criteria=0.0001,
+                           function_set=('add', 'mul', 'sin', 'cos', 'tan'),
                            p_crossover=0.7, p_subtree_mutation=0.1,
                            p_hoist_mutation=0.05, p_point_mutation=0.1,
                            max_samples=0.9, verbose=1,
@@ -32,8 +32,33 @@ est_gp.fit(X_train, y_train)
 print(est_gp._program)
 
 '''
-Result: add(add(mul(sin(X0), sin(X0)), tan(X2)), cos(X1))
+Result: add(
+            add(
+                add(
+                    add(X2, X2), 
+                    mul(X1, X1)
+                    ), 
+                    add(
+                        add(
+                            add(X2, X2), 
+                            add(
+                                add(X2, X2), 
+                                add(
+                                    add(X2, X2), 
+                                    mul(X1, X1)
+                                )
+                            )
+                        ), 
+                        mul(X1, X1)
+                    )
+                ), 
+                mul(
+                    add(X0, X0), 
+                    mul(X0, X0)
+                )
+            )
 
-Result: sin(X0)^2 + cos(X1) + tan(X2)
+Result: y =  X1^2 + (8 * X2 + 2 * X1^2) + (2* X0 * X0^2)
+        y = 2 * X0^3 + 3 * X1^2 + 8 * X2
     
 '''
