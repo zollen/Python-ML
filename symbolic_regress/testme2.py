408c24dfbe4c10d8a8549a69a84b6873fe3ec529
Stephen Kong <zollen.kong@gmail.com>
2024-03-08 06:22:45 -0500

    support subtraction

--- symbolic_regress/testme2.py
+++ symbolic_regress/testme2.py
@@ -13,16 +13,16 @@ rng = check_random_state(0)
 
 # Training samples
 X_train = rng.uniform(-10, 10, 15000).reshape(5000, 3)
-y_train = 2 * X_train[:, 0]**3 + 3 * X_train[:, 1]**2 + 4 * X_train[:, 2]
+y_train = 2 * X_train[:, 0]**3 - 3 * X_train[:, 1]**2 + 4 * X_train[:, 2]
 
 # Testing samples
 X_test = rng.uniform(-10, 10, 15000).reshape(5000, 3)
-y_test = 2 * X_train[:, 0]**3 + 3 * X_train[:, 1]**2 + 4 * X_train[:, 2]
+y_test = 2 * X_train[:, 0]**3 - 3 * X_train[:, 1]**2 + 4 * X_train[:, 2]
 
 
 est_gp = SymbolicRegressor(population_size=10000,
                            generations=40, stopping_criteria=0.0001,
-                           function_set=('add', 'mul', 'sin', 'cos', 'tan'),
+                           function_set=('add', 'mul', 'sub', 'sin', 'cos', 'tan'),
                            p_crossover=0.7, p_subtree_mutation=0.1,
                            p_hoist_mutation=0.05, p_point_mutation=0.1,
                            max_samples=0.9, verbose=1,
