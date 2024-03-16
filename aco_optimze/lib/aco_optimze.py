afe5cad8e40224d12d9869d87de00021805c7247
Stephen Kong <zollen.kong@gmail.com>
2024-03-16 06:47:08 -0400

    refactoring

--- aco_optimze/lib/aco_optimze.py
+++ aco_optimze/lib/aco_optimze.py
@@ -2,6 +2,117 @@
 Created on Mar 12, 2024
 
 @author: STEPHEN
+@url: https://www.youtube.com/watch?v=783ZtAF4j5g
+
+
+A round (r) consists of all ants travel from beginning node(s) to the final destination node(s).
+
+k'th ant travels on the edge(s) i to z <- final destination 
+L is the fixed cost or length of the path all traveled paths 
+--------------------------------------------------------------------
+  
+        z-1,z
+  L(k) =  ∑  fixed_cost(x,y)
+       x=i,y=i+1
+                 
+  all paths(i..z) traveled by the same ant would have the same Δ τ value for that ant  
+  Δ τ(i..z,k) = 1/L(k)
+            
+
+Pheromone values Without vaporization round (r)
+-----------------------------------------------
+               m
+  τ(r,i,j,k) = ∑  Δ τ(r,i,j,k)       
+              k=1
+  
+  
+Pheromone values With vaporization of percentage (p), round (r)
+-----------------------------------------------------------------
+                                      m
+  τ(r+1,i,j,k) = (i - p) * τ(r,i,j) + ∑  Δ τ(r,i,j,k)
+                                     k=1
+                
+Calculating the probabilities
+------------------------------
+  η(i,j) = 1/fixed_cost(i,j)   <== fixed cost or length of the path(i,j)
+  α and β adjust the effectiveness of the components.
+  We could use only the Pheromone for decision making by setting β to 0
+  
+                    τ(r,i,j)^α * η(i,j)^β  
+  P(r+1,i,j) = -------------------------
+                ∑ ( τ(r,i,j)^α * η(i,j)^β )                                       
+                                     
+                                     
+DaemonActions
+--------------
+   Once solutions have been constructed, and before updating the pheromone values, often some problem 
+   specific actions may be required. These are often called daemon actions, and can be used to implement 
+   problem specific and/or centralized actions, which cannot be performed by single ants. The most used 
+   daemon action consists in the application of local search to the constructed solutions: the locally 
+   optimized solutions are then used to decide which pheromone values to update.  
+ 
+ 
+  procedure ACO_MetaHeuristic is
+    init()
+    while not terminated do
+        generateAntsSolutions()  // all ants select paths from beginning to the final destination
+        daemonActions()          // optional
+        pheromoneUpdate()        
+    repeat
+  end procedure
+                                   
+                                  
+ Matrix solution would need cost matrix, pheromone matrix, probabilities matrix     
+ 
+ ant_matrix     
+    i x j   
+ fixed_cost_matrix           
+    i x j                                         
+ delta_pheromone_matrix 
+    i x j
+ pheromone_matrix
+    i x j
+ probabilities_matrix
+    i x j             
+    
+    
+ init()
+    ones(pheromone_matrix)
+     
+ generateAntsSolutions()
+    generate_probabilities()
+        pheromone_matrix, fixed_cost_matrix => probabilities_matrix
+        -----------------------------------------------------------
+            top                       = multiply_element (pheromone_matrix^α, fixed_cost_matrix^β) 
+            bottom                    = horizontal_sum(top)
+            probabilities_matrix      = horizontal_divide(top, bottom)
+            
+    generate_ants_solution()
+        probabilities_matrix => ants_matrix
+        -----------------------------------
+            zeros(ant_matrix)
+            for k in (1..100):
+                i = random([ possible starting locations ])
+                j = -1
+                while j not in [ possible final destinations ]:
+                    j = decide(probabilities_matrix[i])
+                    ant_matrix[k,i,j] = fixed_cost_matrix[i,j]
+                    i = j
+                ant_matrix[k] = 1 / sum(ant_matrix[k])
+              
+        
+ pheromoneUpdate()
+    generate_delta_pheromone()
+        ants_matrix, fixed_cost_matrix => delta_pheromone_matrix 
+        --------------------------------------------------------
+        zeros(delta_pheromone_matrix)
+        for ant_matrix in ants:
+            delta_pheromone_matrix += ant_matrix
+            
+    update_pheromone()
+        pheromone_matrix, delta_pheromone matrix => pheromone_matrix
+        ------------------------------------------------------------
+        pheromone_matrix = (1 - p) * pheromone_matrix + delta_pheromone_matrix
  '''
 import numpy as np
 
