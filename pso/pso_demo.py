8e82e80850733b044cb6766b759cf72dc86a83a1
Stephen Kong <zollen.kong@gmail.com>
2024-03-21 05:49:54 -0400

    cool demo

---
+++ pso/pso_demo.py
@@ -0,0 +1,57 @@
+'''
+Created on Mar 21, 2024
+
+@author: STEPHEN
+@url: https://machinelearningmastery.com/a-gentle-introduction-to-particle-swarm-optimization/
+'''
+
+import numpy as np
+ 
+def f(x,y):
+    "Objective function"
+    return (x-3.14)**2 + (y-2.72)**2 + np.sin(3*x+1.41) + np.sin(4*y-1.73)
+    
+# Compute and plot the function in 3D within [0,5]x[0,5]
+x, y = np.array(np.meshgrid(np.linspace(0,5,100), np.linspace(0,5,100)))
+z = f(x, y)
+ 
+# Find the global minimum
+x_min = x.ravel()[z.argmin()]
+y_min = y.ravel()[z.argmin()]
+ 
+# Hyper-parameter of the algorithm
+c1 = c2 = 0.1
+w = 0.8
+ 
+# Create particles
+n_particles = 20
+np.random.seed(100)
+X = np.random.rand(2, n_particles) * 5
+V = np.random.randn(2, n_particles) * 0.1
+ 
+# Initialize data
+pbest = X
+pbest_obj = f(X[0], X[1])
+gbest = pbest[:, pbest_obj.argmin()]
+gbest_obj = pbest_obj.min()
+ 
+def update():
+    "Function to do one iteration of particle swarm optimization"
+    global V, X, pbest, pbest_obj, gbest, gbest_obj
+    # Update params
+    r1, r2 = np.random.rand(2)
+    V = w * V + c1*r1*(pbest - X) + c2*r2*(gbest.reshape(-1,1)-X)
+    X = X + V
+    obj = f(X[0], X[1])
+    pbest[:, (pbest_obj >= obj)] = X[:, (pbest_obj >= obj)]
+    pbest_obj = np.array([pbest_obj, obj]).min(axis=0)
+    gbest = pbest[:, pbest_obj.argmin()]
+    gbest_obj = pbest_obj.min()
+ 
+
+for i in range(50):
+    update() 
+    print("Round {} found best solution at f({})={}".format(i+1, gbest, gbest_obj))
+ 
+print("PSO found best solution at f({})={}".format(gbest, gbest_obj))
+print("Global optimal at f({})={}".format([x_min,y_min], f(x_min,y_min)))
