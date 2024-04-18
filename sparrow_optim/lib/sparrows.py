6cfc2c43a8cb65dff6667341d2604a89af05d554
Stephen Kong <zollen.kong@gmail.com>
2024-04-18 12:19:16 -0400

    fix bug

--- sparrow_optim/lib/sparrows.py
+++ sparrow_optim/lib/sparrows.py
@@ -117,7 +117,7 @@ class Sparrows:
         ind = np.expand_dims(np.array(range(self.numOfProducers)), axis=1)
         alpha = np.random.rand(producers.shape[0], producers.shape[1])
         Q = np.random.uniform(-1, 1, (producers.shape[0], producers.shape[1]))
-        moves1 = producers * np.e**(ind / (alpha * rounds))
+        moves1 = producers * np.e**(-r / (alpha * rounds))
         moves2 = producers + Q * self.L
         return np.where(r < self.ST, moves1, moves2)
 
