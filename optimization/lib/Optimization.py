18f729b0c8c76163b3599e5f44ba0d31fec90ed3
Stephen Kong <zollen.kong@gmail.com>
2024-04-26 17:30:07 -0400

    code improvement

--- optimization/lib/Optimization.py
+++ optimization/lib/Optimization.py
@@ -63,7 +63,7 @@ class Optimization:
         if self.best_candidates.size == 0:
             pop = self.population
         else:
-            pop = np.vstack((self.population, self.best_candidates, self.pareto_front))
+            pop = np.vstack((self.best_candidates, self.pareto_front, self.population))
         scores = self.fitness(pop)
         self.pareto_front = pop[self.is_pareto_efficient(scores, 'max', False)]
         return self.pareto_front
