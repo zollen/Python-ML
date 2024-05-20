c7543253698e218438ac203c9ee9c0c403fa86ca
Stephen Kong <zollen.kong@gmail.com>
2024-05-20 16:01:12 -0400

    exercise

---
+++ problems/testme2.py
@@ -0,0 +1,21 @@
+'''
+Created on May 20, 2024
+
+@author: STEPHEN
+@url: https://www.sfu.ca/math-coursenotes/Math%20157%20Course%20Notes/sec_Optimization.html
+@desc:
+
+A manufacturer determines that the daily average cost of producing q units is:
+
+C(q)  = 0.0001q^2 - 0.08q + 65 + 5000/q   where q > 0
+C'(q) = 0.0002q - 0.08 - 5000/q^2
+    0.0002q^3 - 0.08q^2 - 5000 = 0
+    q = 500
+c"(Q) = 0.0002 - 10000/q^3
+    10000/q^3 = 0.0002
+    q^3 = 10000/0.002
+    q = 170.997597
+    q > 0, minimum
+
+Therefore, the daily minimum average cost is 500
+'''
