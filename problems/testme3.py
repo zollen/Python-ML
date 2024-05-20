17885235283dd7190040d5c1e0d83f737caeb117
Stephen Kong <zollen.kong@gmail.com>
2024-05-20 17:27:45 -0400

    another exercise

---
+++ problems/testme3.py
@@ -0,0 +1,20 @@
+'''
+Created on May 20, 2024
+
+@author: STEPHEN
+@url: https://www.sfu.ca/math-coursenotes/Math%20157%20Course%20Notes/sec_Optimization.html
+@desc:
+
+It is estimated that the operating rate of a major manufacturer's factories over a certain 365-day 
+period is given by
+
+f(t)  = 100 + (800t) / (t^2 + 90,000)    0 <= t <= 365
+f'(t) = - 800 (t^2 - 90,000) / (t^2 + 90,000)^2 = 0
+      -800 (t^2 - 90,000) = 0
+      t^2 = 90,000
+      t = 300 or -300,    
+      t = 300 because 0 <= t <= 365
+f(300) = 101.308
+
+Thus, the manufacturing capacity operating rate was a maximum after 300 days.
+'''
