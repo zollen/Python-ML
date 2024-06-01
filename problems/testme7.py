a4d5549b2f210e6fbd5a6f6d3a2f288868b4dd95
Stephen Kong <zollen.kong@gmail.com>
2024-06-01 17:08:03 -0400

    exercise

---
+++ problems/testme7.py
@@ -0,0 +1,22 @@
+'''
+Created on May 21, 2024
+
+@author: STEPHEN
+@url: https://www.sfu.ca/math-coursenotes/Math%20157%20Course%20Notes/sec_Optimization.html
+@desc:
+
+
+Of all rectangles of area 100, which has the smallest perimeter?
+
+xy = 100
+P = 2x + 2y
+P(x)  = 2x + 2(100/x)
+P'(x) = 2 - 200 / x^2 = 0
+      2 = 200 / x^2
+      x^2 = 100
+      x = 10
+      y = 10
+P"(X) = 400 / x^3 = 0
+        400 / x^3 = always positive, therefore it is relative minimum
+       
+'''
