d47523e7f3cc35e2c1e2ddaeae3833c3d79f7d09
zollen <zollen.kong@gmail.com>
2024-07-27 15:57:32 -0400

    use 16 threads

--- best_optim/assetstorage3.py
+++ best_optim/assetstorage3.py
@@ -121,7 +121,7 @@ model.Minimize(X + Y)
 
 # Solve problem
 solver = cp_model.CpSolver()
-solver.parameters.num_search_workers = 8
+solver.parameters.num_search_workers = 16
 status = solver.solve(model)
 
 
